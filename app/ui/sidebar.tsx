eb5161081a712501fae4e10dc2ee6f1a04d02154
devitalo97 <dev.italo.souza@gmail.com>
2024-06-20 05:05:54 -0300

    feat(ui): create sidebar and layout component

---
+++ app/ui/sidebar.tsx
@@ -0,0 +1,429 @@
+"use client";
+import {
+  ForwardRefExoticComponent,
+  RefAttributes,
+  SVGProps,
+  useState,
+} from "react";
+import {
+  Dialog,
+  DialogPanel,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import {
+  Bars3Icon,
+  CalendarIcon,
+  ChartPieIcon,
+  DocumentDuplicateIcon,
+  FolderIcon,
+  HomeIcon,
+  UsersIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import { cn } from "../util/cn";
+
+const navigation: {
+  name: string;
+  href?: string;
+  icon: ForwardRefExoticComponent<
+    Omit<SVGProps<SVGSVGElement>, "ref"> & {
+      title?: string | undefined;
+      titleId?: string | undefined;
+    } & RefAttributes<SVGSVGElement>
+  >;
+  current: boolean;
+  children?: {
+    name: string;
+    href: string;
+    current?: boolean;
+  }[];
+}[] = [
+  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
+  {
+    name: "Teams",
+    icon: UsersIcon,
+    current: false,
+    children: [
+      { name: "Engineering", href: "#" },
+      { name: "Human Resources", href: "#" },
+      { name: "Customer Success", href: "#" },
+    ],
+  },
+  {
+    name: "Projects",
+    icon: FolderIcon,
+    current: false,
+    children: [
+      { name: "GraphQL API", href: "#" },
+      { name: "iOS App", href: "#" },
+      { name: "Android App", href: "#" },
+      { name: "New Customer Portal", href: "#" },
+    ],
+  },
+  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
+  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
+  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
+];
+
+const teams = [
+  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
+  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
+  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
+];
+
+export function SideBar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-white">
+        <body class="h-full">
+        ```
+      */}
+      <div>
+        <Transition show={sidebarOpen}>
+          <Dialog className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
+            <TransitionChild
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-900/80" />
+            </TransitionChild>
+
+            <div className="fixed inset-0 flex">
+              <TransitionChild
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <DialogPanel className="relative mr-16 flex w-full max-w-xs flex-1">
+                  <TransitionChild
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                      <button
+                        type="button"
+                        className="-m-2.5 p-2.5"
+                        onClick={() => setSidebarOpen(false)}
+                      >
+                        <span className="sr-only">Close sidebar</span>
+                        <XMarkIcon
+                          className="h-6 w-6 text-white"
+                          aria-hidden="true"
+                        />
+                      </button>
+                    </div>
+                  </TransitionChild>
+                  {/* Sidebar component, swap this element with another sidebar if you like */}
+                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
+                    <div className="flex h-16 shrink-0 items-center">
+                      <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                      />
+                    </div>
+                    <nav className="flex flex-1 flex-col">
+                      <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                        <li>
+                          <ul role="list" className="-mx-2 space-y-1">
+                            {navigation.map((item) => (
+                              <li key={item.name}>
+                                {!item?.children ? (
+                                  <a
+                                    href={item.href}
+                                    className={cn(
+                                      item.current
+                                        ? "bg-gray-50"
+                                        : "hover:bg-gray-50",
+                                      "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700"
+                                    )}
+                                  >
+                                    <item.icon
+                                      className="h-6 w-6 shrink-0 text-gray-400"
+                                      aria-hidden="true"
+                                    />
+                                    {item.name}
+                                  </a>
+                                ) : (
+                                  <Disclosure as="div">
+                                    {({ open }) => (
+                                      <>
+                                        <DisclosureButton
+                                          className={cn(
+                                            item.current
+                                              ? "bg-gray-50"
+                                              : "hover:bg-gray-50",
+                                            "flex w-full items-center gap-x-3 rounded-md p-2 text-left text-sm font-semibold leading-6 text-gray-700"
+                                          )}
+                                        >
+                                          <item.icon
+                                            className="h-6 w-6 shrink-0 text-gray-400"
+                                            aria-hidden="true"
+                                          />
+                                          {item.name}
+                                          <ChevronRightIcon
+                                            className={cn(
+                                              open
+                                                ? "rotate-90 text-gray-500"
+                                                : "text-gray-400",
+                                              "ml-auto h-5 w-5 shrink-0"
+                                            )}
+                                            aria-hidden="true"
+                                          />
+                                        </DisclosureButton>
+                                        <DisclosurePanel
+                                          as="ul"
+                                          className="mt-1 px-2"
+                                        >
+                                          {item?.children?.map((subItem) => (
+                                            <li key={subItem.name}>
+                                              {/* 44px */}
+                                              <DisclosureButton
+                                                as="a"
+                                                href={subItem.href}
+                                                className={cn(
+                                                  subItem?.current
+                                                    ? "bg-gray-50"
+                                                    : "hover:bg-gray-50",
+                                                  "block rounded-md py-2 pl-9 pr-2 text-sm leading-6 text-gray-700"
+                                                )}
+                                              >
+                                                {subItem.name}
+                                              </DisclosureButton>
+                                            </li>
+                                          ))}
+                                        </DisclosurePanel>
+                                      </>
+                                    )}
+                                  </Disclosure>
+                                )}
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                        <li>
+                          <div className="text-xs font-semibold leading-6 text-gray-400">
+                            Your teams
+                          </div>
+                          <ul role="list" className="-mx-2 mt-2 space-y-1">
+                            {teams.map((team) => (
+                              <li key={team.name}>
+                                <a
+                                  href={team.href}
+                                  className={cn(
+                                    team.current
+                                      ? "bg-gray-50 text-indigo-600"
+                                      : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+                                    "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                                  )}
+                                >
+                                  <span
+                                    className={cn(
+                                      team.current
+                                        ? "border-indigo-600 text-indigo-600"
+                                        : "border-gray-200 text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600",
+                                      "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium"
+                                    )}
+                                  >
+                                    {team.initial}
+                                  </span>
+                                  <span className="truncate">{team.name}</span>
+                                </a>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </Dialog>
+        </Transition>
+
+        {/* Static sidebar for desktop */}
+        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+            <div className="flex h-16 shrink-0 items-center">
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company"
+              />
+            </div>
+            <nav className="flex flex-1 flex-col">
+              <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                <li>
+                  <ul role="list" className="-mx-2 space-y-1">
+                    {navigation.map((item) => (
+                      <li key={item.name}>
+                        {!item?.children ? (
+                          <a
+                            href={item.href}
+                            className={cn(
+                              item.current ? "bg-gray-50" : "hover:bg-gray-50",
+                              "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700"
+                            )}
+                          >
+                            <item.icon
+                              className="h-6 w-6 shrink-0 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            {item.name}
+                          </a>
+                        ) : (
+                          <Disclosure as="div">
+                            {({ open }) => (
+                              <>
+                                <DisclosureButton
+                                  className={cn(
+                                    item.current
+                                      ? "bg-gray-50"
+                                      : "hover:bg-gray-50",
+                                    "flex w-full items-center gap-x-3 rounded-md p-2 text-left text-sm font-semibold leading-6 text-gray-700"
+                                  )}
+                                >
+                                  <item.icon
+                                    className="h-6 w-6 shrink-0 text-gray-400"
+                                    aria-hidden="true"
+                                  />
+                                  {item.name}
+                                  <ChevronRightIcon
+                                    className={cn(
+                                      open
+                                        ? "rotate-90 text-gray-500"
+                                        : "text-gray-400",
+                                      "ml-auto h-5 w-5 shrink-0"
+                                    )}
+                                    aria-hidden="true"
+                                  />
+                                </DisclosureButton>
+                                <DisclosurePanel as="ul" className="mt-1 px-2">
+                                  {item?.children?.map((subItem) => (
+                                    <li key={subItem.name}>
+                                      {/* 44px */}
+                                      <DisclosureButton
+                                        as="a"
+                                        href={subItem.href}
+                                        className={cn(
+                                          subItem?.current
+                                            ? "bg-gray-50"
+                                            : "hover:bg-gray-50",
+                                          "block rounded-md py-2 pl-9 pr-2 text-sm leading-6 text-gray-700"
+                                        )}
+                                      >
+                                        {subItem.name}
+                                      </DisclosureButton>
+                                    </li>
+                                  ))}
+                                </DisclosurePanel>
+                              </>
+                            )}
+                          </Disclosure>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li>
+                  <div className="text-xs font-semibold leading-6 text-gray-400">
+                    Your teams
+                  </div>
+                  <ul role="list" className="-mx-2 mt-2 space-y-1">
+                    {teams.map((team) => (
+                      <li key={team.name}>
+                        <a
+                          href={team.href}
+                          className={cn(
+                            team.current
+                              ? "bg-gray-50 text-indigo-600"
+                              : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
+                            "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                          )}
+                        >
+                          <span
+                            className={cn(
+                              team.current
+                                ? "border-indigo-600 text-indigo-600"
+                                : "border-gray-200 text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600",
+                              "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium"
+                            )}
+                          >
+                            {team.initial}
+                          </span>
+                          <span className="truncate">{team.name}</span>
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li className="-mx-6 mt-auto">
+                  <a
+                    href="#"
+                    className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50"
+                  >
+                    <img
+                      className="h-8 w-8 rounded-full bg-gray-50"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                    <span className="sr-only">Your profile</span>
+                    <span aria-hidden="true">Tom Cook</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+            onClick={() => setSidebarOpen(true)}
+          >
+            <span className="sr-only">Open sidebar</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+          <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
+            Dashboard
+          </div>
+          <a href="#">
+            <span className="sr-only">Your profile</span>
+            <img
+              className="h-8 w-8 rounded-full bg-gray-50"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
