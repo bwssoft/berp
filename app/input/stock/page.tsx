f1f27bca18d1ccdb1adc30887875ba006db97058
devitalo97 <dev.italo.souza@gmail.com>
2024-06-23 00:06:35 -0300

    feat(page): add table and charts on stock page

--- app/input/stock/page.tsx
+++ app/input/stock/page.tsx
@@ -1,4 +1,11 @@
+import { getMostRecentStock, updateStock } from "@/app/lib/action/stock.action";
+import { Button } from "@/app/ui/button";
+import { BarChart } from "@/app/ui/chart/bar.chart";
+import StockTable from "@/app/ui/table/stock/table";
+import { ArrowPathIcon } from "@heroicons/react/20/solid";
+
 export default async function Page() {
+  const stock = await getMostRecentStock();
   return (
     <div className="flex flex-col h-full gap-6">
       <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
@@ -11,8 +18,59 @@ export default async function Page() {
             atualizada a cada 24h a partir das transações realizadas no dia.
           </p>
         </div>
+        <form action={updateStock} className="ml-auto">
+          <Button
+            type="submit"
+            className="ml-auto flex items-center gap-x-1 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            <ArrowPathIcon className="-ml-1.5 h-5 w-5" aria-hidden="true" />
+            Atualizar Estoque
+          </Button>
+        </form>
       </div>
-      <div className="mx-auto w-full grid grid-cols-3 grid-rols-1 gap-6"></div>
+      <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8 space-y-12">
+        <StockTable data={stock} />
+      </div>
+      <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8 space-y-12 h-[500px]">
+        <BarChart
+          series={stock.map((s) => ({
+            name: s.input.name,
+            data: [s.cumulative_balance],
+          }))}
+          // series={[
+          //   { data: stock.map((s) => s.cumulative_balance), name: "Acumulado" },
+          // ]}
+          options={{
+            // xaxis: { categories: stock.map((s) => s.input.name) },
+            xaxis: { categories: [""] },
+            chart: { stacked: false },
+            plotOptions: {
+              bar: {
+                horizontal: false,
+              },
+            },
+          }}
+          title="Quantidade de Insumos"
+          subtitle="Valor geral para quantidade de cada insumo do estoque."
+        />
+      </div>
+      <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8 space-y-12">
+        <BarChart
+          series={stock.map((s) => ({
+            name: s.input.name,
+            data: [s.cumulative_balance],
+          }))}
+          options={{
+            xaxis: { categories: [""] },
+            chart: { stacked: true },
+            plotOptions: {
+              bar: {
+                horizontal: true,
+              },
+            },
+          }}
+        />
+      </div>
     </div>
   );
 }
