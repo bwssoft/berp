9db4b906f6860c851a6382b8dc6b88f40eec2428
devitalo97 <dev.italo.souza@gmail.com>
2024-06-25 16:15:29 -0300

    feat(ui): implement defaultValue on Select component

--- app/ui/select.tsx
+++ app/ui/select.tsx
@@ -6,6 +6,7 @@ interface Props<T> extends React.SelectHTMLAttributes<HTMLSelectElement> {
   labelExtractor: (arg: T) => string;
   onChangeSelect?: (arg: T | undefined) => void;
   placeholder?: string;
+  defaultValue?: string;
 }
 
 export function Select<T>(props: Props<T>) {
@@ -16,6 +17,7 @@ export function Select<T>(props: Props<T>) {
     labelExtractor,
     onChangeSelect,
     placeholder,
+    defaultValue,
   } = props;
 
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -32,6 +34,7 @@ export function Select<T>(props: Props<T>) {
       name="input_id"
       className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
       onChange={handleChange}
+      defaultValue={defaultValue}
     >
       {placeholder && <option>{placeholder}</option>}
       {data.map((d) => (
