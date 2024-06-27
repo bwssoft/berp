1be40cfaf26381b80415956b2de28cdedba22f2c
devitalo97 <dev.italo.souza@gmail.com>
2024-06-27 17:35:40 -0300

    feat(ui/table): create tables to Product and ProductionOrder definition

---
+++ app/ui/table/product/columns.tsx
@@ -0,0 +1,49 @@
+import { deleteOneProductById } from "@/app/lib/action";
+import { IProduct } from "@/app/lib/definition";
+import { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+
+export const columns: ColumnDef<IProduct>[] = [
+  { header: "Nome", accessorKey: "name" },
+  {
+    header: "Quant. Insumos",
+    accessorKey: "inputs",
+    cell: ({ row }) => {
+      const product = row.original;
+      return product.inputs.length;
+    },
+  },
+  {
+    header: "Criado em",
+    accessorKey: "created_at",
+    cell: ({ row }) => {
+      const product = row.original;
+      return product.created_at.toLocaleString();
+    },
+  },
+  {
+    header: "",
+    accessorKey: "created_at",
+    cell: ({ row }) => {
+      const product = row.original;
+      return (
+        <td className="flex gap-2 relative whitespace-nowrap pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+          <Link
+            href={`/product/form/update?id=${product.id}`}
+            className="text-indigo-600 hover:text-indigo-900"
+          >
+            Editar
+          </Link>
+          <form action={() => deleteOneProductById({ id: product.id! })}>
+            <button
+              type="submit"
+              className="text-indigo-600 hover:text-indigo-900 px-0 py-0"
+            >
+              Deletar
+            </button>
+          </form>
+        </td>
+      );
+    },
+  },
+];
---
+++ app/ui/table/production-order/columns.tsx
@@ -0,0 +1,64 @@
+import { deleteOneProductionOrderById } from "@/app/lib/action/production-order.action";
+import { IProductionOrder } from "@/app/lib/definition";
+import { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
+
+export const columns: ColumnDef<IProductionOrder>[] = [
+  { header: "Estágio", accessorKey: "stage" },
+  {
+    header: "Tipos de Produtos",
+    accessorKey: "products",
+    cell: ({ row }) => {
+      const productionOrder = row.original;
+      return productionOrder.products.length;
+    },
+  },
+  {
+    header: "Quantidade total",
+    accessorKey: "products",
+    cell: ({ row }) => {
+      const productionOrder = row.original;
+      return productionOrder.products.reduce(
+        (acc, cur) => (acc += cur.quantity),
+        0
+      );
+    },
+  },
+  {
+    header: "Criado em",
+    accessorKey: "created_at",
+    cell: ({ row }) => {
+      const productionOrder = row.original;
+      return productionOrder.created_at.toLocaleString();
+    },
+  },
+  {
+    header: "",
+    accessorKey: "created_at",
+    cell: ({ row }) => {
+      const productionOrder = row.original;
+      return (
+        <td className="flex gap-2 relative whitespace-nowrap pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+          <Link
+            href={`/production-order/form/update?id=${productionOrder.id}`}
+            className="text-indigo-600 hover:text-indigo-900"
+          >
+            Editar
+          </Link>
+          <form
+            action={() =>
+              deleteOneProductionOrderById({ id: productionOrder.id! })
+            }
+          >
+            <button
+              type="submit"
+              className="text-indigo-600 hover:text-indigo-900 px-0 py-0"
+            >
+              Deletar
+            </button>
+          </form>
+        </td>
+      );
+    },
+  },
+];
