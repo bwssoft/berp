5532c2a9ab21c35b141ff4936bb949e5cc13064e
devitalo97 <dev.italo.souza@gmail.com>
2024-07-01 14:03:50 -0300

    feat(lib/utils): implement function to parse data from xlsx file

---
+++ app/lib/util/handle-xlsx-file.ts
@@ -0,0 +1,29 @@
+import ExcelJS from 'exceljs';
+
+export const handleXlsxFile = async <T>(files: File[] | null, callback: (arg: any) => T) => {
+  if (files) {
+    const result: T[] = []
+    for (let file of files) {
+      const workbook = new ExcelJS.Workbook();
+      const arrayBuffer = await file.arrayBuffer();
+      await workbook.xlsx.load(arrayBuffer);
+
+      workbook.eachSheet((sheet) => {
+        const firstRow = sheet.getRow(1)
+        sheet.eachRow((row, rowIdx) => {
+          if (rowIdx === 1) return
+          const obj: { [x: string]: any } = {}
+          row.eachCell((cell, i) => {
+            const _cell = firstRow.getCell(i);
+            const key = _cell.value?.toString()
+            if (!key) return
+            obj[key] = cell.value
+          })
+          result.push(callback(obj));
+        });
+      });
+    };
+    return result
+  }
+};
+
