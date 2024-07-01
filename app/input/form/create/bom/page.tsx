86aeaa13113324c5dea8b9feb160c2dfb10c3b4a
devitalo97 <dev.italo.souza@gmail.com>
2024-07-01 14:06:21 -0300

    feat(page): implement page route to create many input

---
+++ app/input/form/create/bom/page.tsx
@@ -0,0 +1,22 @@
+import InputCreateFromFileForm from "@/app/ui/form/input/input.create-from-file.form";
+
+export default function Page() {
+  return (
+    <div>
+      <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
+        <div>
+          <h1 className="text-base font-semibold leading-7 text-gray-900">
+            Registro de insumos por planilha
+          </h1>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            Baixe o modelo e depois faça o upload da planilha para registrar
+            insumos em massa.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
+        <InputCreateFromFileForm />
+      </div>
+    </div>
+  );
+}
--- app/input/management/page.tsx
+++ app/input/management/page.tsx
@@ -1,6 +1,7 @@
 import { findAllInput } from "@/app/lib/action";
 import InputTable from "@/app/ui/table/input/table";
 import { PlusIcon } from "@heroicons/react/20/solid";
+import { ArrowUpTrayIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 export default async function Example() {
@@ -18,13 +19,23 @@ export default async function Example() {
           </p>
         </div>
 
-        <Link
-          href="/input/form/create"
-          className="ml-auto flex items-center gap-x-1 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          <PlusIcon className="-ml-1.5 h-5 w-5" aria-hidden="true" />
-          Novo insumo
-        </Link>
+        <div className="ml-auto flex gap-6">
+          <Link
+            href="/input/form/create/bom"
+            className="flex items-center gap-x-1 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+          >
+            <ArrowUpTrayIcon className="-ml-1.5 h-5 w-5" aria-hidden="true" />
+            B.O.M
+          </Link>
+
+          <Link
+            href="/input/form/create"
+            className="flex items-center gap-x-1 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            <PlusIcon className="-ml-1.5 h-5 w-5" aria-hidden="true" />
+            Novo insumo
+          </Link>
+        </div>
       </div>
       <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8 space-y-12">
         <InputTable data={inputs} />
