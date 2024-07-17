8dec5c4fc880d3161ada5eae1bd8c4c32bdbb3c2
devitalo97 <dev.italo.souza@gmail.com>
2024-07-17 13:55:19 -0300

    feat(ui/components): add props to increase file upload component reusability

--- app/ui/input-file.tsx
+++ app/ui/input-file.tsx
@@ -3,9 +3,16 @@ import { PhotoIcon } from "@heroicons/react/24/solid";
 
 interface FileUploadProps {
   handleFile: (files: File[] | null) => void;
+  label?: string;
+  id?: string;
 }
 
-export const FileUpload: React.FC<FileUploadProps> = ({ handleFile }) => {
+export const FileUpload: React.FC<FileUploadProps> = ({
+  handleFile,
+  label,
+  id,
+}) => {
+  const inputFileId = id ?? `file-upload-${crypto.randomUUID()}`;
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
@@ -30,12 +37,14 @@ export const FileUpload: React.FC<FileUploadProps> = ({ handleFile }) => {
 
   return (
     <div className="col-span-full">
-      <label
-        htmlFor="cover-photo"
-        className="block text-sm font-medium leading-6 text-gray-900"
-      >
-        Arquivo
-      </label>
+      {label && (
+        <label
+          htmlFor="cover-photo"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          {label}
+        </label>
+      )}
       <div className="mt-2 flex gap-6 justify-center align-bottom rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
         <div className="text-center">
           <PhotoIcon
@@ -44,12 +53,12 @@ export const FileUpload: React.FC<FileUploadProps> = ({ handleFile }) => {
           />
           <div className="mt-4 flex text-sm leading-6 text-gray-600">
             <label
-              htmlFor="file-upload"
+              htmlFor={inputFileId}
               className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
             >
               <span>Selecione os arquivos</span>
               <input
-                id="file-upload"
+                id={inputFileId}
                 type="file"
                 className="sr-only"
                 multiple
