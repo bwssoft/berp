f54b74e807c04c287d5ef170051cfcd04a1c4b47
devitalo97 <dev.italo.souza@gmail.com>
2024-07-17 15:27:04 -0300

    style(page): modify icon height and width

--- app/input/management/page.tsx
+++ app/input/management/page.tsx
@@ -24,7 +24,7 @@ export default async function Example() {
             href="/input/form/create/bom"
             className="flex items-center gap-x-1 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
           >
-            <ArrowUpTrayIcon className="-ml-1.5 h-5 w-5" aria-hidden="true" />
+            <ArrowUpTrayIcon className="-ml-1.5 h-4 w-4" aria-hidden="true" />
             B.O.M
           </Link>
 
