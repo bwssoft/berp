f904b29c41c34027498f73cfecd7785c901d7a9e
devitalo97 <dev.italo.souza@gmail.com>
2024-07-23 15:15:21 -0300

    feat(domain): declare device domain

---
+++ app/lib/@backend/domain/device/repository/device.repository.interface.ts
@@ -0,0 +1,4 @@
+import { IBaseRepository } from "../../@shared/repository/repository.interface";
+import { IDevice } from "../entity";
+
+export interface IDeviceRepository extends IBaseRepository<IDevice> { }
