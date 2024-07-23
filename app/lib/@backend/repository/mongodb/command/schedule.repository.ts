34fe27fda297333c4e8f666fb8edd13d7cd5f05d
devitalo97 <dev.italo.souza@gmail.com>
2024-07-23 15:21:21 -0300

    feat(repository): implement command repository

---
+++ app/lib/@backend/repository/mongodb/command/schedule.repository.ts
@@ -0,0 +1,14 @@
+import { singleton } from "@/app/lib/util/singleton";
+import { ISchedule } from "@/app/lib/@backend/domain";
+import { BaseRepository } from "../@base/base";
+
+class ScheduleRepository extends BaseRepository<ISchedule> {
+  constructor() {
+    super({
+      collection: "command-schedule",
+      db: "berp"
+    });
+  }
+}
+
+export const scheduleRepository = singleton(ScheduleRepository)
