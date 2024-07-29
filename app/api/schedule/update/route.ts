a9f83eb3fe488ae47c19e1617ba08591390dfe1a
devitalo97 <dev.italo.souza@gmail.com>
2024-07-29 13:15:19 -0300

    feat(api): check query object before execute usecase

--- app/api/schedule/update/route.ts
+++ app/api/schedule/update/route.ts
@@ -1,9 +1,19 @@
-import { updateOneScheduleById } from "@/app/lib/@backend/action"
+import { updateManyScheduleById } from "@/app/lib/@backend/action"
 
 export async function PATCH(request: Request) {
   const { query, value } = await request.json()
 
-  await updateOneScheduleById(query, value)
+  if (!("id" in query)) return new Response(
+    "No 'id' on query",
+    { status: 400 }
+  )
 
-  return new Response("ok")
+  if (!("pending" in value)) return new Response(
+    "No 'pending' on value",
+    { status: 400 }
+  )
+
+  await updateManyScheduleById(query, value)
+
+  return Response.json({ ok: true })
 }
