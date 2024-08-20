f04db417e292d7e691a8b6615039737131ac50b9
Nathan985 <nathan.rodrigu3s@gmail.com>
2024-08-20 17:02:25 -0300

    feat: interface

--- app/lib/@backend/usecase/webhook/client/usecases/create.client.usecase.ts
+++ app/lib/@backend/usecase/webhook/client/usecases/create.client.usecase.ts
@@ -3,6 +3,6 @@ import { ICreateClientUseCase } from "@/app/lib/@backend/domain/webhook/client/u
 export class CreateClientUseCase implements ICreateClientUseCase {
 
   public async execute(data: ICreateClientUseCase.Execute.Params): Promise<ICreateClientUseCase.Execute.Result> {
-    
+
   }
 }
