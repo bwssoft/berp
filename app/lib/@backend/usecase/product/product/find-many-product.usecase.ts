e3cfb247a34ac38a3bf9dc83b00c4be704e55c60
devitalo97 <dev.italo.souza@gmail.com>
2024-08-22 09:01:19 -0300

    feat(usecase): implement find-many-product usecase

---
+++ app/lib/@backend/usecase/product/product/find-many-product.usecase.ts
@@ -0,0 +1,24 @@
+import { singleton } from "@/app/lib/util/singleton"
+import { IProduct, IProductRepository } from "@/app/lib/@backend/domain"
+import { productRepository } from "@/app/lib/@backend/repository/mongodb"
+import { Filter } from "mongodb"
+
+class FindManyProductUsecase {
+  repository: IProductRepository
+
+  constructor() {
+    this.repository = productRepository
+  }
+
+  async execute(input: Partial<IProduct>) {
+    const pipeline = this.pipeline(input)
+    const aggregate = await this.repository.aggregate(pipeline)
+    return await aggregate.toArray() as IProduct[]
+  }
+
+  pipeline(input: Filter<IProduct>) {
+    return [{ $match: input }]
+  }
+}
+
+export const findManyProductUsecase = singleton(FindManyProductUsecase)
