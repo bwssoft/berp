76d8bc34e65cb1564e5d9de9d7ed6c78cf3140d5
devitalo97 <dev.italo.souza@gmail.com>
2024-08-23 16:07:28 -0300

    feat(api): check if body has property 'ping'

--- app/api/webhook/omie/client/manage/route.ts
+++ app/api/webhook/omie/client/manage/route.ts
@@ -2,6 +2,9 @@ import { ClientController } from "@/app/lib/@backend/controller/client/client.co
 
 export async function POST(request: Request) {
   const data = await request.json();
+
+  if ("ping" in data) return new Response("Pong", { status: 200 })
+
   const clientController = new ClientController();
   await clientController.execute(data);
 
