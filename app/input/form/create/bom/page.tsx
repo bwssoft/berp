8ff1f03a0538618bfe7858b152e748a6242b02e6
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2024-09-02 09:40:32 -0300

    feat: add download input bom form usage

--- app/input/form/create/bom/page.tsx
+++ app/input/form/create/bom/page.tsx
@@ -1,4 +1,5 @@
 import { InputCreateFromFileForm } from "@/app/lib/@frontend/ui";
+import { DownloadInputBOMForm } from "@/app/lib/@frontend/ui/form/input/download-input-category-bom.form";
 
 export default function Page() {
   return (
@@ -8,16 +9,10 @@ export default function Page() {
           <h1 className="text-base font-semibold leading-7 text-gray-900">
             Registro de insumos por planilha
           </h1>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            <a
-              href={"/xlsx/create-input-from-file.xlsx"}
-              download={"create-input-from-file.xlsx"}
-              className="text-indigo-600 hover:text-indigo-500 underline underline-offset-2 hover:cursor-pointer"
-            >
-              Baixe o modelo
-            </a>{" "}
-            e depois faça o upload da planilha para registrar insumos em massa.
-          </p>
+          <div className="flex items-center gap-1 mt-1 text-sm leading-6 text-gray-600">
+            <DownloadInputBOMForm />
+            <p>e depois faça o upload da planilha para registrar insumos em massa.</p>
+          </div>
         </div>
       </div>
       <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
