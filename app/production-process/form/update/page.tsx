d044d697a60cddcabe795225d70ec8bc30c4ca86
Giovane <giovanedaniell@gmail.com>
2024-09-12 14:47:59 -0300

    fix: wrong labels

--- app/production-process/form/update/page.tsx
+++ app/production-process/form/update/page.tsx
@@ -18,7 +18,7 @@ export default async function Page(props: Props) {
         <div className="flex flex-wrap items-center gap-6 px-4 sm:fPlex-nowrap sm:px-6 lg:px-8">
           <div>
             <h1 className="text-base font-semibold leading-7 text-gray-900">
-              Nenhuma Ordem de Produção encontrado
+              Nenhum processo de produção encontrado
             </h1>
           </div>
         </div>
@@ -31,7 +31,7 @@ export default async function Page(props: Props) {
       <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
         <div>
           <h1 className="text-base font-semibold leading-7 text-gray-900">
-            Atualização de ordem de produção
+            Atualização de processo de produção
           </h1>
           <p className="mt-1 text-sm leading-6 text-gray-600">
             Preencha o formulário abaixo para atualizar uma ordem de produção.
