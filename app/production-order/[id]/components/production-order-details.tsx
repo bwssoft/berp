70074792bfcca49db194ae225369c2b2562228f8
Giovane <giovanedaniell@gmail.com>
2024-09-17 11:18:17 -0300

    feat: add production order details content

--- app/production-order/[id]/components/production-order-details.tsx
+++ app/production-order/[id]/components/production-order-details.tsx
@@ -1,9 +1,83 @@
 "use client";
 
-export function ProductionOrderDetails() {
+import {
+  IProductionOrder,
+  productionOrderPriorityMapping,
+  productionOrderStageMapping,
+} from "@/app/lib/@backend/domain";
+import { formatDate } from "@/app/lib/util/format-date";
+
+type ProductionOrderDetailsProps = {
+  productionOrder: IProductionOrder | null;
+};
+
+export function ProductionOrderDetails({
+  productionOrder,
+}: ProductionOrderDetailsProps) {
+  if (!productionOrder)
+    return <p>Dados da ordem de produção não encontrados.</p>;
+
   return (
-    <div>
-      <p>Production order details</p>
+    <div className="p-2">
+      <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          Ordem de produção
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+          Aba contendo informações da ordem de produção
+        </p>
+      </div>
+
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Identificador
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {productionOrder.id}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Estágio de produção
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {productionOrderStageMapping[productionOrder.stage]}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Prioridade
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {productionOrderPriorityMapping[productionOrder.priority]}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Descrição
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {productionOrder.description}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Data de criação
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {formatDate(new Date(productionOrder.created_at), {
+                includeHours: true,
+              })}
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
   );
 }
