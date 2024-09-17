013cb00ab100677a396b2bc68224ae6cae3a5834
Giovane <giovanedaniell@gmail.com>
2024-09-17 11:18:31 -0300

    feat: add client details content

--- app/production-order/[id]/components/client-details.tsx
+++ app/production-order/[id]/components/client-details.tsx
@@ -1,9 +1,133 @@
 "use client";
 
-export function ClientDetails() {
+import {
+  clientSectorMapping,
+  clientTypeMapping,
+  IClient,
+} from "@/app/lib/@backend/domain";
+import { formatDate } from "@/app/lib/util/format-date";
+
+type ClientDetailsProps = {
+  client: IClient | null;
+};
+
+export function ClientDetails({ client }: ClientDetailsProps) {
+  if (!client) return <p>Dados do cliente não encontrados.</p>;
+
+  const clientBillingAddresss = client.billing_address;
+
   return (
-    <div>
-      <p>Client details</p>
+    <div className="p-2">
+      <div className="px-4 sm:px-0">
+        <h3 className="text-base font-semibold leading-7 text-gray-900">
+          Cliente
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+          Aba contendo informações do cliente
+        </p>
+      </div>
+
+      <div className="mt-6 border-t border-gray-100">
+        <dl className="divide-y divide-gray-100">
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Identificador
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {client.id}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Documento{" "}
+              <span className="text-gray-400 font-normal">
+                ({client.document.type})
+              </span>
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {client.document.value}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Nome
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {client.corporate_name}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Registro municipal
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {client.municipal_registration}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Registro estadual
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {client.municipal_registration}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Descrição
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {client.description}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Classificação
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {clientTypeMapping[client.type]}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Setor / ramo
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {clientSectorMapping[client.sector]}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Endereço de cobrança
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              Rua {clientBillingAddresss.street}, {clientBillingAddresss.city},{" "}
+              {clientBillingAddresss.state} -{" "}
+              {clientBillingAddresss.postal_code} -{" "}
+              {clientBillingAddresss.country}
+            </dd>
+          </div>
+
+          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+            <dt className="text-sm font-medium leading-6 text-gray-900">
+              Data de criação
+            </dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+              {formatDate(new Date(client.created_at), {
+                includeHours: true,
+              })}
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
   );
 }
