7ef5d70d4fe283b94997856310d563e76ef4bac3
devitalo97 <dev.italo.souza@gmail.com>
2024-09-23 09:56:15 -0300

    force

--- app/firmware/form/update/page.tsx
+++ app/firmware/form/update/page.tsx
@@ -17,7 +17,7 @@ export default async function Page(props: Props) {
         <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
           <div>
             <h1 className="text-base font-semibold leading-7 text-gray-900">
-              Nenhum firmware encontrado
+              Nenhum firmware foi encontrado
             </h1>
           </div>
         </div>
