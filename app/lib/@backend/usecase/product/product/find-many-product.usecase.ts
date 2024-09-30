e81c412437dc558ac46b8c67c0f37b9c96ba4d6a
Giovane <giovanedaniell@gmail.com>
2024-09-30 14:57:32 -0300

    fix: remove unnecessary aggregation step on find many product usecase

--- app/lib/@backend/usecase/product/product/find-many-product.usecase.ts
+++ app/lib/@backend/usecase/product/product/find-many-product.usecase.ts
@@ -17,59 +17,7 @@ class FindManyProductUsecase {
   }
 
   pipeline(input: Filter<IProduct>) {
-    return [
-      { $match: input },
-      {
-        $lookup: {
-          from: "technical-sheet",
-          foreignField: "id",
-          localField: "technical_sheet_id",
-          as: "technical_sheets"
-        }
-      },
-      {
-        $project: {
-          name: 1,
-          description: 1,
-          color: 1,
-          created_at: 1,
-          id: 1,
-          technical_sheets: 1,
-          omie_code_metadata: 1,
-          input_id: {
-            $reduce: {
-              input: "$technical_sheets",
-              initialValue: [],
-              in: {
-                $concatArrays: [
-                  "$$value",
-                  {
-                    $map: {
-                      input: "$$this.inputs",
-                      as: "input",
-                      in: "$$input.uuid"
-                    }
-                  }
-                ]
-              }
-            }
-          }
-        }
-      },
-      {
-        $lookup: {
-          from: "input",
-          localField: "input_id",
-          foreignField: "id",
-          as: "inputs"
-        }
-      },
-      {
-        $project: {
-          input_id: 0
-        }
-      }
-    ]
+    return [{ $match: input }];
   }
 }
 
