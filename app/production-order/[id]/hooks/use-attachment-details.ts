195fb76cec760172175ce478fdf671baf9ee9885
Giovane <giovanedaniell@gmail.com>
2024-10-11 16:00:19 -0300

    fix: downloading attachment not working

--- app/production-order/[id]/hooks/use-attachment-details.ts
+++ app/production-order/[id]/hooks/use-attachment-details.ts
@@ -1,6 +1,5 @@
 import { getSaleOrderAttachmentUrl } from "@/app/lib/@backend/action/omie/attachments/get-sale-order-attachment-url";
 import { OmieEnterpriseEnum } from "@/app/lib/@backend/domain/@shared/gateway/omie/omie.gateway.interface";
-import axios from "axios";
 
 export function useAttachmentDetails() {
   async function downloadAttachment(params: {
@@ -12,23 +11,12 @@ export function useAttachmentDetails() {
   }) {
     const url = await getSaleOrderAttachmentUrl(params);
 
-    console.log({ url });
-
-    const response = await axios.get(url, {
-      responseType: "blob",
-    });
-
-    const blob = new Blob([response.data], {
-      type: response.headers["content-type"],
-    });
-
     const link = document.createElement("a");
 
-    link.href = window.URL.createObjectURL(blob);
+    link.href = url;
     link.download = url.split("/").pop() || "downloaded_file";
+    link.target = "_blank";
     link.click();
-
-    window.URL.revokeObjectURL(link.href);
   }
 
   return {
