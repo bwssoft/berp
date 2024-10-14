226766cf9606f1566560ac63cf3903a184b7d486
Giovane <giovanedaniell@gmail.com>
2024-10-14 12:46:15 -0300

    feat: change omie status when production order is finished

---
+++ app/lib/@backend/action/omie/sale-order/update-sale-order-status.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { OmieEnterpriseEnum } from "../../../domain/@shared/gateway/omie/omie.gateway.interface";
+import { OmieSaleOrderStage } from "../../../domain/@shared/webhook/omie/omie-sale-order.webhook.interface";
+import { saleOrderOmieGateway } from "../../../gateway/omie";
+
+type GetSaleOrderAttachmentUrlParams = {
+  saleOrderId: string;
+  statusId: OmieSaleOrderStage;
+  enterprise: keyof typeof OmieEnterpriseEnum;
+};
+
+export async function updateSaleOrderStatus(
+  params: GetSaleOrderAttachmentUrlParams
+) {
+  saleOrderOmieGateway.setSecrets(OmieEnterpriseEnum[params.enterprise]);
+
+  saleOrderOmieGateway.updateStage(params.saleOrderId, params.statusId);
+}
