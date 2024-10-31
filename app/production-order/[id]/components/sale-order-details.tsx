85197d56a9d17fd3a63ba76201fab5f66a21d98e
Giovane <giovanedaniell@gmail.com>
2024-10-31 11:00:04 -0300

    feat: remove unnecessary data from sale order details

--- app/production-order/[id]/components/sale-order-details.tsx
+++ app/production-order/[id]/components/sale-order-details.tsx
@@ -61,15 +61,6 @@ export function SaleOrderDetails({ saleOrder }: SaleOrderDetailsProps) {
 
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">
-              Id da ordem de serviço (OMIE)
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              {saleOrder.omie_webhook_metadata.order_id}
-            </dd>
-          </div>
-
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
               Etapa
             </dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
