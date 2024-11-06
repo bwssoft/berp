361d707943d1d6bd59b2b7bc6db1913eac0d3e43
Nathan985 <nathan.rodrigu3s@gmail.com>
2024-11-06 15:38:14 -0300

    feat: create pie chart

--- app/production-order/dashboard/page.tsx
+++ app/production-order/dashboard/page.tsx
@@ -1,114 +1,167 @@
 import { findAllProductionOrderWithProduct } from "@/app/lib/@backend/action";
 import { BarChart } from "@/app/lib/@frontend/ui";
+import PieChart from "@/app/lib/@frontend/ui/chart/pie.chart";
 import { productionOrderConstants } from "@/app/lib/constant";
 
 export default async function Page() {
-  const productionOrders = await findAllProductionOrderWithProduct();
+    const productionOrders = await findAllProductionOrderWithProduct();
 
-  const productionOrdersByStage = productionOrders.reduce((acc, current) => {
-    acc[productionOrderConstants.stage[current.stage]] = (acc[productionOrderConstants.stage[current.stage]] ?? 0) + 1
+    const productionOrdersByStage = productionOrders.reduce((acc, current) => {
+        acc[productionOrderConstants.stage[current.stage]] =
+            (acc[productionOrderConstants.stage[current.stage]] ?? 0) + 1;
 
-    return acc
-  }, {} as Record<string, number>)
+        return acc;
+    }, {} as Record<string, number>);
 
-  const productsQuantity = productionOrders.reduce((acc, current) => {
-    const productsWithQuantities = current.sale_order.products
-    const currentItemProductsMetadata = current.products_in_sale_order
+    const productsQuantity = productionOrders.reduce((acc, current) => {
+        const productsWithQuantities = current.sale_order.products;
+        const currentItemProductsMetadata = current.products_in_sale_order;
 
-    productsWithQuantities.forEach((product) => {
-      const productMetadata = currentItemProductsMetadata.find((productMetadata) => productMetadata.id === product.product_id)!
+        productsWithQuantities.forEach((product) => {
+            const productMetadata = currentItemProductsMetadata.find(
+                (productMetadata) => productMetadata.id === product.product_id
+            )!;
 
-      const stage = productionOrderConstants.stage[current.stage]
+            const stage = productionOrderConstants.stage[current.stage];
 
-      if (!acc[stage]) {
-        acc[stage] = {}
-      }
+            if (!acc[stage]) {
+                acc[stage] = {};
+            }
 
-      acc[stage][productMetadata.name] = (acc[stage][productMetadata.name] ?? 0) + product.quantity
-    })
+            acc[stage][productMetadata.name] =
+                (acc[stage][productMetadata.name] ?? 0) + product.quantity;
+        });
 
-    return acc
-  }, {} as Record<string, Record<string, number>>)
+        return acc;
+    }, {} as Record<string, Record<string, number>>);
 
-  // spreads since sort mutates the array in memory reference
-  const productionOrdersWithMostProductsQuantity = [...productionOrders].sort((po1, po2) => {
-    const productionOrder1ProductsQuantity = po1.sale_order.products.reduce((acc, curr) => acc + curr.quantity, 0)
-    const productionOrder2ProductsQuantity = po2.sale_order.products.reduce((acc, curr) => acc + curr.quantity, 0)
+    // spreads since sort mutates the array in memory reference
+    const productionOrdersWithMostProductsQuantity = [...productionOrders]
+        .sort((po1, po2) => {
+            const productionOrder1ProductsQuantity =
+                po1.sale_order.products.reduce(
+                    (acc, curr) => acc + curr.quantity,
+                    0
+                );
+            const productionOrder2ProductsQuantity =
+                po2.sale_order.products.reduce(
+                    (acc, curr) => acc + curr.quantity,
+                    0
+                );
 
-    return productionOrder2ProductsQuantity - productionOrder1ProductsQuantity
-  }).slice(0, 5)
-  
-  const finishedProductionOrdersProductsQuantity = productionOrders
-    .filter((productionOrder) => productionOrder.stage === 'completed')
-    .reduce((acc, current) => {
-      const productsWithQuantities = current.sale_order.products
-      const currentItemProductsMetadata = current.products_in_sale_order
+            return (
+                productionOrder2ProductsQuantity -
+                productionOrder1ProductsQuantity
+            );
+        })
+        .slice(0, 5);
 
-      productsWithQuantities.forEach((product) => {
-        const productMetadata = currentItemProductsMetadata.find((productMetadata) => productMetadata.id === product.product_id)!
+    const finishedProductionOrdersProductsQuantity = productionOrders
+        .filter((productionOrder) => productionOrder.stage === "completed")
+        .reduce((acc, current) => {
+            const productsWithQuantities = current.sale_order.products;
+            const currentItemProductsMetadata = current.products_in_sale_order;
 
-        acc[productMetadata.name] = (acc[productMetadata.name] ?? 0) + product.quantity
-      })
+            productsWithQuantities.forEach((product) => {
+                const productMetadata = currentItemProductsMetadata.find(
+                    (productMetadata) =>
+                        productMetadata.id === product.product_id
+                )!;
 
-      return acc
-    }, {} as Record<string, number>)
+                acc[productMetadata.name] =
+                    (acc[productMetadata.name] ?? 0) + product.quantity;
+            });
 
-  return (
-    <div>
-      <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
+            return acc;
+        }, {} as Record<string, number>);
+
+        console.log({
+          keys: Object.keys(finishedProductionOrdersProductsQuantity),
+        })
+
+    return (
         <div>
-          <h1 className="text-base font-semibold leading-7 text-gray-900">
-            Dashboard
-          </h1>
-          <p className="mt-2 text-sm text-gray-700">
-            Dashboard para insights sobre as ordens de produção 
-          </p>
-        </div> 
-      </div>
+            <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
+                <div>
+                    <h1 className="text-base font-semibold leading-7 text-gray-900">
+                        Dashboard
+                    </h1>
+                    <p className="mt-2 text-sm text-gray-700">
+                        Dashboard para insights sobre as ordens de produção
+                    </p>
+                </div>
+            </div>
 
-      <div className="grid grid-cols-2 mt-8 grid-rows-auto items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
-        <BarChart
-          subtitle="Quantidade de ordens produção por estágio"
-          series={Object.entries(productionOrdersByStage).map(([key, value]) => ({
-            name: key,
-            data: [value],
-            color: '#4f46e5', // bg-indigo-600
-          }))}
-          options={{
-            xaxis: { categories: [""] },
-            chart: { stacked: false },
-            plotOptions: {
-              bar: {
-                horizontal: true,
-              },
-            },
-            stroke: {
-              width: 1,
-            },
-          }}
-        />
+            <div className="grid grid-cols-2 mt-8 grid-rows-auto items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
+                <BarChart
+                    subtitle="Quantidade de ordens produção por estágio"
+                    series={Object.entries(productionOrdersByStage).map(
+                        ([key, value]) => ({
+                            name: key,
+                            data: [value],
+                            color: "#4f46e5", // bg-indigo-600
+                        })
+                    )}
+                    options={{
+                        xaxis: { categories: [""] },
+                        chart: { stacked: false },
+                        plotOptions: {
+                            bar: {
+                                horizontal: true,
+                            },
+                        },
+                        stroke: {
+                            width: 1,
+                        },
+                    }}
+                />
 
-        <BarChart
-          subtitle="Ordens de produção com mais produtos"
-          series={productionOrdersWithMostProductsQuantity.map((productionOrder) => ({
-            name: `OP-${productionOrder.code.toString().padStart(5, "0")}`,
-            data: [productionOrder.sale_order.products.reduce((acc, curr) => acc + curr.quantity, 0)],
-            color: '#4f46e5', // bg-indigo-600
-          }))}
-          options={{
-            xaxis: { categories: [""] },
-            chart: { stacked: false },
-            plotOptions: {
-              bar: {
-                horizontal: false,
-              },
-            },
-            stroke: {
-              width: 1,
-            },
-          }}
-        />
-      </div>
-    </div>
-  )
+                <BarChart
+                    subtitle="Ordens de produção com mais produtos"
+                    series={productionOrdersWithMostProductsQuantity.map(
+                        (productionOrder) => ({
+                            name: `OP-${productionOrder.code
+                                .toString()
+                                .padStart(5, "0")}`,
+                            data: [
+                                productionOrder.sale_order.products.reduce(
+                                    (acc, curr) => acc + curr.quantity,
+                                    0
+                                ),
+                            ],
+                            color: "#4f46e5", // bg-indigo-600
+                        })
+                    )}
+                    options={{
+                        xaxis: { categories: [""] },
+                        chart: { stacked: false },
+                        plotOptions: {
+                            bar: {
+                                horizontal: false,
+                            },
+                        },
+                        stroke: {
+                            width: 1,
+                        },
+                    }}
+                />
+
+                <PieChart
+                    subtitle="Quantidade de equipamento por modelo"
+                    options={{
+                      labels: Object.keys(finishedProductionOrdersProductsQuantity),
+                      colors: Object.keys(finishedProductionOrdersProductsQuantity).map(() => "#4f46e5"),
+                      stroke: {
+                        width: 2,
+                        show: true,
+                      },
+                      markers: {
+                        colors: "#4f46e5",
+                      }
+                    }}
+                    series={Object.values(finishedProductionOrdersProductsQuantity)}
+                />
+            </div>
+        </div>
+    );
 }
