f243e726e9a354f4708b670cca54b7b1bd021cfe
Giovane <giovanedaniell@gmail.com>
2024-11-07 09:28:07 -0300

    feat: add revalidate

--- app/lib/@backend/action/production-order.action.ts
+++ app/lib/@backend/action/production-order.action.ts
@@ -1,43 +1,56 @@
-"use server"
+"use server";
 
-import { revalidatePath } from "next/cache"
-import { IProduct, IProductionOrder, ISaleOrder } from "../domain"
+import { revalidatePath } from "next/cache";
+import { IProduct, IProductionOrder, ISaleOrder } from "../domain";
 import {
   createOneProductionOrderUsecase,
-  findOneProductionOrderUsecase,
   deleteOneProductionOrderUsecase,
-  updateOneProductionOrderUsecase,
   findAllProductionOrderUsecase,
-  findAllProductionOrderWithProductUsecase
-} from "../usecase"
+  findAllProductionOrderWithProductUsecase,
+  findOneProductionOrderUsecase,
+  updateOneProductionOrderUsecase,
+} from "../usecase";
 
-
-export async function createOneProductionOrder(productionOrder: Omit<IProductionOrder, "id" | "created_at">) {
-  await createOneProductionOrderUsecase.execute(productionOrder)
-  revalidatePath("/production-order/management")
+export async function createOneProductionOrder(
+  productionOrder: Omit<IProductionOrder, "id" | "created_at">
+) {
+  await createOneProductionOrderUsecase.execute(productionOrder);
+  revalidatePath("/production-order/management");
 }
 
-export async function findOneProductionOrder(productionOrder: Partial<IProductionOrder>) {
-  return await findOneProductionOrderUsecase.execute(productionOrder)
+export async function findOneProductionOrder(
+  productionOrder: Partial<IProductionOrder>
+) {
+  return await findOneProductionOrderUsecase.execute(productionOrder);
 }
 
-export async function updateOneProductionOrderById(query: { id: string }, value: Omit<Partial<IProductionOrder>, "id" | "created_at">) {
-  await updateOneProductionOrderUsecase.execute(query, value)
-  revalidatePath("/production-order/management")
-  revalidatePath("/production-order/kanban")
+export async function updateOneProductionOrderById(
+  query: { id: string },
+  value: Omit<Partial<IProductionOrder>, "id" | "created_at">
+) {
+  await updateOneProductionOrderUsecase.execute(query, value);
+  revalidatePath("/production-order/management");
+  revalidatePath("/production-order/kanban");
+  revalidatePath("/production-order/dashboard");
 }
 
 export async function deleteOneProductionOrderById(query: { id: string }) {
-  await deleteOneProductionOrderUsecase.execute(query)
-  revalidatePath("/production-order/management")
-  revalidatePath("/production-order/kanban")
+  await deleteOneProductionOrderUsecase.execute(query);
+  revalidatePath("/production-order/management");
+  revalidatePath("/production-order/kanban");
 }
 
 export async function findAllProductionOrder(): Promise<IProductionOrder[]> {
-  return await findAllProductionOrderUsecase.execute()
+  return await findAllProductionOrderUsecase.execute();
 }
 
-export async function findAllProductionOrderWithProduct(input?: Partial<IProductionOrder>): Promise<(IProductionOrder & { sale_order: ISaleOrder, products_in_sale_order: IProduct[] })[]> {
-  return await findAllProductionOrderWithProductUsecase.execute(input)
+export async function findAllProductionOrderWithProduct(
+  input?: Partial<IProductionOrder>
+): Promise<
+  (IProductionOrder & {
+    sale_order: ISaleOrder;
+    products_in_sale_order: IProduct[];
+  })[]
+> {
+  return await findAllProductionOrderWithProductUsecase.execute(input);
 }
-
