3c2723cf310572e2f23df1770601780df42694fe
devitalo97 <dev.italo.souza@gmail.com>
2025-01-02 13:29:01 -0300

    feat(usecase): bring 'enterprise', 'product' and 'input' objects with lookup

--- app/lib/@backend/usecase/production/production-order/find-one-production-order.usecase.ts
+++ app/lib/@backend/usecase/production/production-order/find-one-production-order.usecase.ts
@@ -1,4 +1,5 @@
 import {
+  IInput,
   IProduct,
   IProductionOrder,
   IProductionOrderRepository,
@@ -6,11 +7,31 @@ import {
 import { productionOrderRepository } from "@/app/lib/@backend/infra";
 import { singleton } from "@/app/lib/util/singleton";
 import { RemoveMongoId } from "@/app/lib/@backend/decorators";
+import { IEnterprise } from "../../../domain/business/entity/enterprise.entity";
 
 namespace Dto {
   export interface Input extends Partial<IProductionOrder> { }
-  export type Document = IProductionOrder & { product: IProduct }
-  export type Output = IProductionOrder & { product: IProduct } | undefined
+  export type Document = IProductionOrder & {
+    enterprise: Pick<IEnterprise, "id" | "short_name">,
+    product: Pick<IProduct, "id" | "name" | "bom" | "process_execution" | "created_at" | "color" | "description">,
+    input: Pick<IInput, "id" | "name">[]
+  }
+  export type Output = IProductionOrder & { product: Product, enterprise: Pick<IEnterprise, "id" | "short_name"> } | undefined
+  export interface Product {
+    id: string
+    name: string
+    color: string
+    description: string
+    created_at: Date
+    process_execution?: IProduct["process_execution"]
+    bom?: {
+      input: {
+        id: string
+        name: string
+      }
+      quantity: number
+    }[]
+  }
 }
 
 class FindOneProductionOrderUsecase {
@@ -21,10 +42,38 @@ class FindOneProductionOrderUsecase {
   }
 
   @RemoveMongoId()
-  async execute(input: Dto.Input): Promise<Dto.Output> {
-    const aggregate = await this.repository.aggregate<Dto.Document>(this.pipeline(input))
+  async execute(arg: Dto.Input): Promise<Dto.Output> {
+    const aggregate = await this.repository.aggregate<Dto.Document>(this.pipeline(arg))
     const [production_order] = await aggregate.toArray()
-    return production_order
+
+    if (!production_order) return undefined
+
+    const { input: input_array, product, ...rest } = production_order
+
+    const transformedProduct: Dto.Product = {
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      color: product.color,
+      created_at: product.created_at,
+      process_execution: product.process_execution,
+      bom: product?.bom?.map((bomItem) => {
+        const matchedInput = input_array.find((inp) => inp.id === bomItem.input_id);
+        if (!matchedInput) return undefined
+        return {
+          input: {
+            id: bomItem.input_id,
+            name: matchedInput?.name!,
+          },
+          quantity: bomItem.quantity
+        };
+      }).filter((el): el is NonNullable<typeof el> => el !== undefined)
+    };
+
+    return {
+      ...rest,
+      product: transformedProduct
+    }
   }
 
   pipeline(input: Partial<IProductionOrder>) {
@@ -34,16 +83,61 @@ class FindOneProductionOrderUsecase {
       },
       {
         $lookup: {
+          as: "enterprise",
+          from: "business-enterprise",
+          foreignField: "id",
+          localField: "enterprise_id",
+          pipeline: [
+            {
+              $project: {
+                _id: 0,
+                id: 1,
+                short_name: 1,
+              }
+            }
+          ]
+        }
+      },
+      {
+        $lookup: {
           as: "product",
           from: "product",
           foreignField: "id",
-          localField: "product_id"
+          localField: "product_id",
+          pipeline: [
+            {
+              $project: {
+                _id: 0,
+                id: 1,
+                name: 1,
+                bom: 1,
+                process_execution: 1,
+                color: 1,
+                description: 1,
+                created_at: 1
+              }
+            }
+          ]
+        }
+      },
+      {
+        $lookup: {
+          as: "input",
+          from: "input",
+          foreignField: "id",
+          localField: "product.bom.input_id",
+          pipeline: [
+            {
+              $project: { _id: 0, id: 1, name: 1 }
+            }
+          ]
         }
       },
       {
         $project: {
           product: { $first: "$product" },
-          created_at: 1,
+          enterprise: { $first: "$enterprise" },
+          input: 1,
           id: 1,
           client_id: 1,
           proposal_id: 1,
@@ -52,13 +146,12 @@ class FindOneProductionOrderUsecase {
           line_items: 1,
           product_id: 1,
           total_quantity: 1,
-          production_process_id: 1,
-          production_execution_id: 1,
           code: 1,
-          priority: 1,
           stage: 1,
+          priority: 1,
+          created_at: 1,
         }
-      },
+      }
     ]
   }
 }
