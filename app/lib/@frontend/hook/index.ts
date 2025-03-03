a49571fb11a265af4a28329c3fea3bbbf5230ffd
devitalo97 <dev.italo.souza@gmail.com>
2025-03-03 12:52:14 -0300

    feat: export hooks

--- app/lib/@frontend/hook/index.ts
+++ app/lib/@frontend/hook/index.ts
@@ -2,3 +2,5 @@ export * from "./is-on-pathname";
 export * from "./use-debounce";
 export * from "./use-media-query";
 export * from "./use-toast";
+export * from "./use-detect-wrong-imei";
+export * from "./use-E34G-communication";
