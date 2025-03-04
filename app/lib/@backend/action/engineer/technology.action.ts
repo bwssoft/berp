0b5c45e9536d8941c25de99e5c8ff6c6fd41e1f0
devitalo97 <dev.italo.souza@gmail.com>
2025-03-04 18:32:35 -0300

    feat: add params on 'findManyTechnology' server action

--- app/lib/@backend/action/engineer/technology.action.ts
+++ app/lib/@backend/action/engineer/technology.action.ts
@@ -1,13 +1,16 @@
 "use server";
 
+import { Filter } from "mongodb";
 import { ITechnology } from "../../domain";
 import {
   findManyTechnologyUsecase,
   findOneTechnologyUsecase,
 } from "../../usecase";
 
-export async function findManyTechnology(): Promise<ITechnology[]> {
-  return await findManyTechnologyUsecase.execute();
+export async function findManyTechnology(
+  input: Filter<ITechnology>
+): Promise<ITechnology[]> {
+  return await findManyTechnologyUsecase.execute(input);
 }
 
 export async function findOneTechnology(
