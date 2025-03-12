f6f91e29ff0b9f534ba9b35d75e29c3b204ca594
devitalo97 <dev.italo.souza@gmail.com>
2025-03-12 12:07:47 -0300

    feat: add dependencies on useEffect

--- app/lib/@frontend/hook/use-serial-port.ts
+++ app/lib/@frontend/hook/use-serial-port.ts
@@ -43,7 +43,7 @@ export const useSerialPort = (props: Props) => {
       };
     }
     // }
-  }, [handleDisconnection]);
+  }, [handleConnection, handleDisconnection]);
 
   //Serial
   const requestPort = async () => {
