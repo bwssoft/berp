12bcea48b287b54c037dce34767c738d193584f0
devitalo97 <dev.italo.souza@gmail.com>
2025-03-12 12:11:27 -0300

    feat: implement create-many usecase

--- app/lib/@backend/action/production/configuration-log.action.ts
+++ app/lib/@backend/action/production/configuration-log.action.ts
@@ -1,15 +1,24 @@
 "use server";
 
 import { IConfigurationLog } from "@/app/lib/@backend/domain";
-import { createOneConfigurationLogUsecase } from "@/app/lib/@backend/usecase";
-import { revalidatePath } from "next/cache";
+import {
+  createManyConfigurationLogUsecase,
+  createOneConfigurationLogUsecase,
+} from "@/app/lib/@backend/usecase";
 
 export async function createOneConfigurationLog(
-  configurationLog: Omit<IConfigurationLog, "id" | "created_at" | "user_id">
+  input: Omit<IConfigurationLog, "id" | "created_at" | "user_id">
 ) {
   await createOneConfigurationLogUsecase.execute({
-    ...configurationLog,
+    ...input,
     user_id: crypto.randomUUID(),
   });
-  revalidatePath("/production/configuration-log");
+}
+
+export async function createManyConfigurationLog(
+  input: Omit<IConfigurationLog, "id" | "created_at" | "user_id">[]
+) {
+  await createManyConfigurationLogUsecase.execute(
+    input.map((i) => ({ ...i, user_id: crypto.randomUUID() }))
+  );
 }
