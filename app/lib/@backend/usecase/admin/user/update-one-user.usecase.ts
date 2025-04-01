19dbc5025da815f6028a34bd61344acdf6187247
diego-dps <diego.pereira2806@hotmail.com>
2025-04-01 16:15:59 -0300

    feat: update user

---
+++ app/lib/@backend/usecase/admin/user/update-one-user.usecase.ts
@@ -0,0 +1,21 @@
+import { singleton } from "@/app/lib/util/singleton";
+import { IUserRepository } from "../../../domain/admin/repository/user.repository.interface";
+import { userRepository } from "../../../infra/repository/mongodb/admin/user.repository";
+import { IUser } from "../../../domain/admin/entity/user.definition";
+
+class UpdateOneUserUsecase {
+    repository: IUserRepository;
+
+    constructor() {
+        this.repository = userRepository;
+    }
+
+    async execute(
+        query: { id: string },
+        value: Omit<IUser, "id" | "created_at">
+    ) {
+        return await this.repository.updateOne(query, { $set: value });
+    }
+}
+
+export const updateOneUserUsecase = singleton(UpdateOneUserUsecase);
