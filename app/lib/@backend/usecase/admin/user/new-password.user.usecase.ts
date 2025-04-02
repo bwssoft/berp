ca30ed95bf40b4e7ebf367e43d2cf93ad45766dc
Lauane Oliveira <oliveiralauane91@gmail.com>
2025-04-02 14:33:28 -0300

    feat: add password hashing

--- app/lib/@backend/usecase/admin/user/new-password.user.usecase.ts
+++ app/lib/@backend/usecase/admin/user/new-password.user.usecase.ts
@@ -1,6 +1,8 @@
 import { singleton } from "@/app/lib/util";
 import { IUserRepository } from "../../../domain/admin/repository/user.repository.interface"
 import { userRepository } from "../../../infra/repository/mongodb/admin/user.repository";
+import { hash } from "bcrypt";
+import { randomInt } from "crypto";
 
 namespace Dto {
     export type Input = { id: string; password: string } 
@@ -17,8 +19,11 @@ class NewPasswordUserUsecase {
     async execute(input: Dto.Input): Promise<Dto.Output> {
         const { id, password } = input;
 
+        const salt = randomInt(10, 16);
+        const hashedPassword = await hash(password, salt);
+
         try {
-            await this.repository.updateOne({id}, { $set: { password } });
+            await this.repository.updateOne({id}, { $set: { password: hashedPassword } });
             return { success: true };
         } catch (err) {
             console.error(err);
