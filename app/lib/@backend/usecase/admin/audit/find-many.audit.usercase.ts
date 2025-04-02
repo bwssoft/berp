9cc5ee34c8fa18e08dddc5335cfc431e7bacff2e
diego-dps <diego.pereira2806@hotmail.com>
2025-04-02 08:34:05 -0300

    feat: add find many to audit

---
+++ app/lib/@backend/usecase/admin/audit/find-many.audit.usercase.ts
@@ -0,0 +1,22 @@
+import { Filter } from "mongodb";
+
+import { IAudit } from "@/app/lib/@backend/domain";
+import { singleton } from "@/app/lib/util/singleton";
+import { RemoveMongoId } from "@/app/lib/@backend/decorators";
+import { auditRepository } from "../../../infra/repository/mongodb/admin/audit.repository";
+
+export namespace Dto {
+    export type Input = Filter<IAudit>;
+    export type Output = IAudit[];
+}
+
+class FindManyAuditUsecase {
+    repository = auditRepository;
+
+    @RemoveMongoId()
+    async execute(input: Dto.Input): Promise<Dto.Output> {
+        return await this.repository.findAll(input);
+    }
+}
+
+export const findManyAuditUsecase = singleton(FindManyAuditUsecase);
