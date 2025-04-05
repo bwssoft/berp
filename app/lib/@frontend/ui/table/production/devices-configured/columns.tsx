5556b425d9c5554057ff49d64d71343547f822e2
devitalo97 <dev.italo.souza@gmail.com>
2025-04-04 23:11:36 -0300

    feat: use Button component on action column

--- app/lib/@frontend/ui/table/production/devices-configured/columns.tsx
+++ app/lib/@frontend/ui/table/production/devices-configured/columns.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/app/lib/util";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import { ColumnDef } from "@tanstack/react-table";
 import Link from "next/link";
+import { Button } from "../../../component";
 
 const statuses = {
   progress: "text-gray-500 bg-gray-800/20",
@@ -119,14 +120,10 @@ export const columns: ColumnDef<{
     cell: ({ row }) => {
       const { original } = row;
       return (
-        <Link
-          href={`/production/configurator/log/${original.id}`}
-          className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-        >
-          <ArrowTopRightOnSquareIcon
-            aria-hidden="true"
-            className="-ml-0.5 size-5"
-          />
+        <Link href={`/production/log/configurator/${original.id}`}>
+          <Button variant="ghost">
+            <ArrowTopRightOnSquareIcon aria-hidden="true" className="size-5" />
+          </Button>
         </Link>
       );
     },
