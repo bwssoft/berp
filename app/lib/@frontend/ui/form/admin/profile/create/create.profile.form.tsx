fcb5d019762e26ad999415b5896e8d2bf0e2c665
diego-dps <diego.pereira2806@hotmail.com>
2025-04-10 10:43:07 -0300

    fix: fix profile create form

--- app/lib/@frontend/ui/form/admin/profile/create/create.profile.form.tsx
+++ app/lib/@frontend/ui/form/admin/profile/create/create.profile.form.tsx
@@ -3,11 +3,11 @@ import { Button } from "@/app/lib/@frontend/ui/component/button";
 import { useCreateProfileForm } from "./use-create.device.form";
 
 export function CreateProfileForm() {
-  const { handleSubmit, register } = useCreateProfileForm();
+  const { handleSubmit, register, handleBackPage, reset } = useCreateProfileForm();
   return (
     <form
       action={() => handleSubmit()}
-      className="bg-white px-4 sm:px-6 lg:px-8 rounded-md pb-6 shadow-sm ring-1 ring-inset ring-gray-900/10"
+      className="bg-white w-full px-4 sm:px-6 lg:px-8 rounded-md pb-6 shadow-sm ring-1 ring-inset ring-gray-900/10"
     >
       <div className="border-b border-gray-900/10 pb-6">
         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -37,19 +37,28 @@ export function CreateProfileForm() {
         </div>
       </div>
 
-      <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button
-          type="button"
-          className="text-sm font-semibold leading-6 text-gray-900"
-        >
-          Cancelar
-        </button>
-        <Button
-          type="submit"
-          className="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-        >
-          Salvar
-        </Button>
+      <div className="mt-6 flex items-center justify-between gap-x-6">
+
+        <div>
+           <Button variant="ghost" onClick={handleBackPage} type="button">
+              Voltar
+            </Button>
+        </div>
+        <div className="flex gap-2">
+            <Button variant="secondary" 
+            onClick={(event) => {
+              event.preventDefault();
+              reset();
+            }} type="button">
+              Cancelar
+            </Button>
+          <Button
+            type="submit"
+            variant="default"
+          >
+            Salvar
+          </Button>
+        </div>
       </div>
     </form>
   );
--- app/lib/@frontend/ui/form/admin/profile/create/use-create.device.form.ts
+++ app/lib/@frontend/ui/form/admin/profile/create/use-create.device.form.ts
@@ -1,55 +1,61 @@
 import { createOneProfile, findManyControl } from "@/app/lib/@backend/action";
 import { toast } from "@/app/lib/@frontend/hook/use-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const schema = z.object({
-  name: z.string().min(1, "Esse campo não pode ser vazio"),
-  active: z.boolean().default(true),
-  locked_control_code: z.array(z.string()).default([]),
+    name: z.string().min(1, "Esse campo não pode ser vazio"),
+    active: z.boolean().default(true),
+    locked_control_code: z.array(z.string()).default([]),
 });
 
 export type Schema = z.infer<typeof schema>;
 
 export function useCreateProfileForm() {
-  const {
-    register,
-    handleSubmit: hookFormSubmit,
-    formState: { errors },
-    control,
-    setValue,
-    reset: hookFormReset,
-  } = useForm<Schema>({
-    resolver: zodResolver(schema),
-  });
+    const {
+        register,
+        handleSubmit: hookFormSubmit,
+        formState: { errors },
+        control,
+        setValue,
+        reset: hookFormReset,
+    } = useForm<Schema>({
+        resolver: zodResolver(schema),
+    });
+    const router = useRouter();
+    const handleSubmit = hookFormSubmit(async (data) => {
+        try {
+            //fazer a request
+            const controls = await findManyControl({}, 200);
+            data.locked_control_code = controls.map(({ code }) => code);
+            await createOneProfile(data);
+            toast({
+                title: "Sucesso!",
+                description: "Perfil registrado com sucesso!",
+                variant: "success",
+            });
+        } catch (e) {
+            toast({
+                title: "Erro!",
+                description: "Falha ao registrar o perfil!",
+                variant: "error",
+            });
+        }
+    });
 
-  const handleSubmit = hookFormSubmit(async (data) => {
-    try {
-      //fazer a request
-      const controls = await findManyControl({}, 200);
-      data.locked_control_code = controls.map(({ code }) => code);
-      await createOneProfile(data);
-      toast({
-        title: "Sucesso!",
-        description: "Perfil registrado com sucesso!",
-        variant: "success",
-      });
-    } catch (e) {
-      toast({
-        title: "Erro!",
-        description: "Falha ao registrar o perfil!",
-        variant: "error",
-      });
+    function handleBackPage() {
+        router.back();
     }
-  });
 
-  return {
-    register,
-    handleSubmit,
-    errors,
-    control,
-    setValue,
-    reset: hookFormReset,
-  };
+    return {
+        register,
+        handleBackPage,
+        handleSubmit,
+        errors,
+        control,
+        setValue,
+        reset: hookFormReset,
+    };
 }
