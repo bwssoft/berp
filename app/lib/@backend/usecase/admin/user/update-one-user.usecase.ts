7bf8243a08fde1c39be1101a9c035a5fb6814dc8
diego-dps <diego.pereira2806@hotmail.com>
2025-04-10 16:25:46 -0300

    feat: add validation

--- app/lib/@backend/usecase/admin/user/update-one-user.usecase.ts
+++ app/lib/@backend/usecase/admin/user/update-one-user.usecase.ts
@@ -13,7 +13,44 @@ class UpdateOneUserUsecase {
         query: { id: string },
         value: Partial<Omit<IUser, "id" | "created_at">>
     ) {
-        return await this.repository.updateOne(query, { $set: value });
+        const emailExists = await this.repository.findOne({
+            email: value.email,
+            id: { $ne: query.id },
+        });
+        if (emailExists)
+            return {
+                success: false,
+                error: { email: "Email já cadastrado para outro usuário!" },
+            };
+
+        const cpfExists = await this.repository.findOne({
+            cpf: value.cpf,
+            id: { $ne: query.id },
+        });
+        if (cpfExists)
+            return {
+                success: false,
+                error: { email: "CPF já cadastrado em outro usuário!" },
+            };
+
+        const userNameExists = await this.repository.findOne({
+            username: value.username,
+            id: { $ne: query.id },
+        });
+        if (userNameExists)
+            return {
+                success: false,
+                error: { email: "Username já cadastrado para outro usuário!" },
+            };
+
+        try {
+            return await this.repository.updateOne(query, { $set: value });
+        } catch (error) {
+            return {
+                success: false,
+                error: error,
+            };
+        }
     }
 }
 
