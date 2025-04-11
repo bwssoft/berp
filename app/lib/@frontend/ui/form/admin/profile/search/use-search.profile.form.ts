2cb2bb27f970f69f29aa86c2a41e71792e26e3ef
diego-dps <diego.pereira2806@hotmail.com>
2025-04-11 17:12:54 -0300

    feat: add modal on search profile form

--- app/lib/@frontend/ui/form/admin/profile/search/use-search.profile.form.ts
+++ app/lib/@frontend/ui/form/admin/profile/search/use-search.profile.form.ts
@@ -1,24 +1,104 @@
 "use client";
 
+import { useState, ChangeEvent } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useQuery } from "@tanstack/react-query";
+
 import { useDebounce, useHandleParamsChange } from "@/app/lib/@frontend/hook";
-import { useCallback, ChangeEvent } from "react";
+import { findManyProfile } from "@/app/lib/@backend/action";
+
+const schema = z.object({
+    quick: z.string().optional(),
+
+    profile_id: z.array(z.string()).optional(),
+    username: z.string().optional(),
+    active: z
+        .array(
+            z.object({
+                id: z.string(),
+                name: z.string(),
+                value: z.boolean(),
+            })
+        )
+        .optional(),
+});
+
+export type SearchProfileFormValues = z.infer<typeof schema>;
 
 export const useSearchProfileForm = () => {
-  const { handleParamsChange } = useHandleParamsChange();
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleChangeProfileName = useDebounce(
-    useCallback(
-      (e: ChangeEvent<HTMLInputElement>) => {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        setValue,
+        control,
+        watch,
+        formState: { errors },
+    } = useForm<SearchProfileFormValues>({
+        resolver: zodResolver(schema),
+        defaultValues: {
+            profile_id: [],
+            username: "",
+            active: undefined,
+        },
+    });
+
+    const { handleParamsChange } = useHandleParamsChange();
+
+    const { data: profiles = [] } = useQuery({
+        queryKey: ["findManyProfiles"],
+        queryFn: () => findManyProfile({}),
+    });
+
+    const toggleModal = () => setIsModalOpen((prev) => !prev);
+
+    const onSubmit = handleSubmit((data) => {
+        const params: Record<
+            string,
+            string | boolean | (string | boolean)[] | undefined
+        > = {
+            profile_id: data.profile_id,
+            username: data.username,
+            active: data.active?.map(({ value }) => value),
+        };
+
+        handleParamsChange(params);
+        toggleModal();
+    });
+
+    const onReset = () => {
+        reset();
         handleParamsChange({
-          profile_name: e.target.value,
+            profile_id: "",
+            username: "",
+            active: undefined,
         });
-      },
-      [handleParamsChange]
-    ),
-    300
-  );
+    };
 
-  return {
-    handleChangeProfileName,
-  };
+    const handleChangeProfileName = useDebounce(
+        (e: ChangeEvent<HTMLInputElement>) => {
+            handleParamsChange({
+                profile_name: e.target.value,
+            });
+        },
+        300
+    );
+
+    return {
+        register,
+        control,
+        setValue,
+        watch,
+        errors,
+        isModalOpen,
+        toggleModal,
+        profiles,
+        onSubmit,
+        onReset,
+        handleChangeProfileName,
+    };
 };
