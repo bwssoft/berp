c234fc740682b509f4395fdd66a25f8db9bdd345
devitalo97 <dev.italo.souza@gmail.com>
2025-04-10 21:46:53 -0300

    feat: add function to handle logic to show option to user

--- app/lib/@frontend/context/auth.context.tsx
+++ app/lib/@frontend/context/auth.context.tsx
@@ -1,19 +1,36 @@
 "use client";
 
-import React, { createContext, useContext } from "react";
+import React, {
+  ComponentType,
+  createContext,
+  SVGProps,
+  useCallback,
+  useContext,
+} from "react";
 import { IProfile, IUser } from "../../@backend/domain";
 import { useSession } from "next-auth/react";
 import { Session } from "next-auth";
+import { NavItem } from "../ui/component";
 
-type AuthUser = Partial<IUser> & { current_profile: IProfile };
+namespace Context {
+  type User = Partial<IUser> & { current_profile: IProfile };
 
-interface AuthContextType {
-  user: AuthUser | undefined;
-  profile: IProfile | undefined;
-  changeProfile: (input: IProfile) => void;
+  export interface AuthContextType {
+    user: User | undefined;
+    profile: IProfile | undefined;
+    changeProfile: (input: IProfile) => void;
+    navigationByProfile: (input: NavigationItem[]) => NavigationItem[];
+  }
+
+  export interface NavigationItem extends NavItem {
+    code: string;
+    children?: NavigationItem[];
+  }
 }
 
-const AuthContext = createContext<AuthContextType>({} as AuthContextType);
+const AuthContext = createContext<Context.AuthContextType>(
+  {} as Context.AuthContextType
+);
 
 export const AuthProvider = ({
   session,
@@ -28,12 +45,23 @@ export const AuthProvider = ({
       user: { ...data?.user, current_profile: input },
     });
   };
+  const navigationByProfile = useCallback(
+    (navigation: Context.NavigationItem[]): Context.NavigationItem[] => {
+      return navigation.filter((n) =>
+        n.children
+          ? navigationByProfile(navigation)
+          : data?.user.current_profile.locked_control_code.includes(n.code)
+      );
+    },
+    [data]
+  );
   return (
     <AuthContext.Provider
       value={{
         user: data?.user ?? session?.user,
         profile: data?.user?.current_profile ?? session?.user?.current_profile,
         changeProfile,
+        navigationByProfile,
       }}
     >
       {children}
