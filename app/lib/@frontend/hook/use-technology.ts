d282579e9fdcba3ff5e078e0f6c40bcd3fae1fdd
devitalo97 <dev.italo.souza@gmail.com>
2025-04-15 20:29:12 -0300

    feat: add lora auto test

--- app/lib/@frontend/hook/use-technology.ts
+++ app/lib/@frontend/hook/use-technology.ts
@@ -1,18 +1,22 @@
 import { ITechnology } from "../../@backend/domain";
 import { useE3Plus } from "./use-e3-plus";
 import { useE3Plus4G } from "./use-e3-plus-4g";
+import { useLora } from "./use-lora";
 import { useNB2 } from "./use-nb-2";
 
 export const useTechnology = (technology: ITechnology | null) => {
   const e3Plus = useE3Plus();
   const e3Plus4G = useE3Plus4G();
   const nb2 = useNB2();
+  const lora = useLora();
   if (technology?.name.system === "DM_E3_PLUS_4G") {
     return e3Plus4G;
   } else if (technology?.name.system === "DM_E3_PLUS") {
     return e3Plus;
   } else if (technology?.name.system === "DM_BWS_NB2") {
     return nb2;
+  } else if (technology?.name.system === "DM_BWS_LORA") {
+    return lora;
   } else {
     return e3Plus4G;
   }
