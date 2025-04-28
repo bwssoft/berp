afce8b2e33e5cbcc247efddd360b6daeefd428f3
Lauane Oliveira <oliveiralauane91@gmail.com>
2025-04-28 14:22:32 -0300

    fix: change findMany for default pattern

--- app/lib/@backend/usecase/admin/user/find-many.user.usecase.ts
+++ app/lib/@backend/usecase/admin/user/find-many.user.usecase.ts
@@ -4,7 +4,17 @@ import type { Filter } from "mongodb";
 import { userRepository } from "@/app/lib/@backend/infra";
 import { IUser, IUserRepository } from "@/app/lib/@backend/domain";
 import { RemoveFields } from "@/app/lib/@backend/decorators";
+import { PaginationResult } from "../../../domain/@shared/repository/pagination.interface";
 
+namespace Dto {
+    export interface Input {
+        filter?: Filter<IUser>;
+        page?: number;
+        limit?: number;
+        sort?: Record<string, 1 | -1>;
+    }
+  export type Output = PaginationResult<IUser>;
+}
 class FindManyUserUsecase {
     repository: IUserRepository;
 
