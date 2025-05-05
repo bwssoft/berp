119718bafcd78837248aa33d2e4153429eb822af
devitalo97 <dev.italo.souza@gmail.com>
2025-05-05 09:27:29 -0300

    refactor: remove prop from useQuery

--- app/lib/@frontend/ui/modal/admin/profile/user-linked/use-user-linked.profile.modal.ts
+++ app/lib/@frontend/ui/modal/admin/profile/user-linked/use-user-linked.profile.modal.ts
@@ -2,7 +2,7 @@
 import { findManyUser } from "@/app/lib/@backend/action";
 import { IProfile, IUser } from "@/app/lib/@backend/domain";
 import { PaginationResult } from "@/app/lib/@backend/domain/@shared/repository/pagination.interface";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useCallback, useState } from "react";
 
 const PAGE_SIZE = 10;
@@ -16,8 +16,6 @@ export function useUserLinkedProfileModal() {
 
   const [currentPage, setCurrentPage] = useState(1);
 
-  const queryClient = useQueryClient();
-
   const handleProfileSelection = useCallback((profile: Pick<IProfile, "id" | "name">) => {
     setProfile(profile);
     setOpen(true);
@@ -27,24 +25,19 @@ export function useUserLinkedProfileModal() {
   const { data: users = { docs: [], total: 0, pages: 1 }, isLoading } = useQuery({
     queryKey: ["findManyUsers", profile, currentPage], 
     queryFn: async () => {
-      if (!profile?.name) return { docs: [], total: 0, pages: 1 };
+      if (!profile) return { docs: [], total: 0, pages: 1 };
 
       const filter = {
         "profile.name": profile.name,
       };
 
-      const { docs, total, pages, limit } = await findManyUser(filter, currentPage, PAGE_SIZE);
-
-      return { docs, total, pages, limit }; 
-    },
-    enabled: !!profile,
-    initialData: { docs: [], total: 0, pages: 1 }
+      return await findManyUser(filter, currentPage, PAGE_SIZE);
+    }
   });
 
   // Função para alterar a páginação
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
-    queryClient.invalidateQueries({ queryKey: ["findManyUsers", profile, page] });
   };
 
   return {
