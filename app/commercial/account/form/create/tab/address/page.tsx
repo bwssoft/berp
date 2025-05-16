2727392f13ba9e6997bfa63f03660eb6e9fbe67e
diego-dps <diego.pereira2806@hotmail.com>
2025-05-16 10:52:25 -0300

    feat: add adress page

--- app/commercial/account/form/create/tab/address/page.tsx
+++ app/commercial/account/form/create/tab/address/page.tsx
@@ -1,20 +1,69 @@
-import { AccountCreateForm } from "@/app/lib/@frontend/ui/component";
+"use client";
+import { IAddress } from "@/app/lib/@backend/domain";
+import { Button } from "@/app/lib/@frontend/ui/component";
+import { AddressCardList } from "@/app/lib/@frontend/ui/list/comercial/address/address.list";
+import { AddressModal } from "@/app/lib/@frontend/ui/modal/comercial/address";
+import { useAddressModal } from "@/app/lib/@frontend/ui/modal/comercial/address/use-address.modal";
 
-export default async function Page() {
+export function AddressForm() {
+    const { openModal, open } = useAddressModal();
+
+    const mockIAddresses: IAddress[] = [
+        {
+            id: "a1b2c3d4",
+            street: "Maple Avenue",
+            district: "Greenwood",
+            city: "Springfield",
+            state: "California",
+            zip_code: "90210",
+            number: "742",
+            complement: "Apartment 5B",
+            reference_point: "Near the old library",
+            type: "Residencial",
+            created_at: new Date("2024-10-15T10:30:00Z"),
+            updated_at: new Date("2025-04-22T15:45:00Z"),
+        },
+        {
+            id: "a1b2c3d4",
+            street: "Maple Avenue",
+            district: "Greenwood",
+            city: "Springfield",
+            state: "California",
+            zip_code: "90210",
+            number: "742",
+            complement: "Apartment 5B",
+            reference_point: "Near the old library",
+            type: "Residencial",
+            created_at: new Date("2024-10-15T10:30:00Z"),
+            updated_at: new Date("2025-04-22T15:45:00Z"),
+        },
+        {
+            id: "a1b2c3d4",
+            street: "Maple Avenue",
+            district: "Greenwood",
+            city: "Springfield",
+            state: "California",
+            zip_code: "90210",
+            number: "742",
+            complement: "Apartment 5B",
+            reference_point: "Near the old library",
+            type: "Residencial",
+            created_at: new Date("2024-10-15T10:30:00Z"),
+            updated_at: new Date("2025-04-22T15:45:00Z"),
+        },
+    ];
+
     return (
         <div>
-            <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
-                <div>
-                    <h1 className="text-base font-semibold leading-7 text-gray-900">
-                        Registro de conta
-                    </h1>
-                    <p className="mt-1 text-sm leading-6 text-gray-600">
-                        Preencha o formulário abaixo para registrar uma conta.
-                    </p>
+            <AddressModal open={open} />
+            <div className="flex justify-between items-start gap-4">
+                <div className="flex flex-wrap gap-2">
+                    <AddressCardList items={mockIAddresses} />
                 </div>
-            </div>
-            <div className="flex flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
-                <AccountCreateForm />
+
+                <Button onClick={openModal} type="button" className="h-fit">
+                    Novo
+                </Button>
             </div>
         </div>
     );
