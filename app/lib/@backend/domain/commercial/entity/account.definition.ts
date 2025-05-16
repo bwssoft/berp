93fc9a203dd51e31262121ffd6dae4b733effe6d
diego-dps <diego.pereira2806@hotmail.com>
2025-05-16 11:16:24 -0300

    feat: add account  interface

--- app/lib/@backend/domain/commercial/entity/account.definition.ts
+++ app/lib/@backend/domain/commercial/entity/account.definition.ts
@@ -1,1 +1,22 @@
-export interface IAccount extends Record<string, any> {}
+import { IAddress } from "./address.definition";
+import { IContact } from "./contact.definition";
+import { ISector } from "./sector.definition";
+
+export interface IAccount {
+    id?: string | undefined;
+    document: { value: string; type: "cpf" | "cnpj" };
+    name?: string;
+    rg?: string;
+    social_name?: string;
+    fantasy_name?: string;
+    state_registration?: string;
+    municipal_registration?: string;
+    status?: string;
+    setor?: ISector[];
+    address?: IAddress[];
+    contact?: IContact[];
+    economic_group_holding?: string;
+    economic_group_controlled?: string;
+    created_at?: Date;
+    updated_at?: Date;
+}
