b381ee625371658b6280890b8b1cd5af90cc38e4
diego-dps <diego.pereira2806@hotmail.com>
2025-05-21 14:54:31 -0300

    feat: add sector update action

--- app/lib/@backend/usecase/commercial/sector/update-one.sector.usecase.ts
+++ app/lib/@backend/usecase/commercial/sector/update-one.sector.usecase.ts
@@ -5,7 +5,7 @@ import { singleton } from "@/app/lib/util/singleton";
 import { RemoveMongoId } from "@/app/lib/@backend/decorators";
 import { ISector, ISectorRepository } from "@/app/lib/@backend/domain";
 
-class UpdateOneAccountUsecase {
+class UpdateOneSectorUsecase {
     repository: ISectorRepository;
 
     constructor() {
@@ -13,9 +13,16 @@ class UpdateOneAccountUsecase {
     }
 
     @RemoveMongoId()
-    async execute(filter: Filter<ISector>, update: Partial<ISector>) {
-        return await this.repository.updateOne(filter, update);
+    async execute(
+        filter: Filter<ISector>,
+        update: Partial<ISector>
+    ): Promise<ISector> {
+        await this.repository.updateOne(filter, update);
+        const updated = await this.repository.findOne(filter);
+        if (!updated) throw new Error("Setor não encontrado após update");
+
+        return updated;
     }
 }
 
-export const updateOneAccountUsecase = singleton(UpdateOneAccountUsecase);
+export const updateOneSectorUsecase = singleton(UpdateOneSectorUsecase);
