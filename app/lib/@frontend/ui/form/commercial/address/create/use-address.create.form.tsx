2e28eb3505f6726db65d995296c23052c066461a
diego-dps <diego.pereira2806@hotmail.com>
2025-05-23 14:28:19 -0300

    fix: reload address page

--- app/lib/@frontend/ui/form/commercial/address/create/use-address.create.form.tsx
+++ app/lib/@frontend/ui/form/commercial/address/create/use-address.create.form.tsx
@@ -7,115 +7,116 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "@/app/lib/@frontend/hook";
 import { createOneAddress } from "@/app/lib/@backend/action";
 import { isValidCEP } from "@/app/lib/util/is-valid-cep";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const AddressFormSchema = z.object({
-  zip_code: z.string().min(8, "CEP obrigatório").refine(isValidCEP, {
-    message: "CEP inválido.",
-  }),
-  street: z.string().min(1, "Logradouro obrigatório"),
-  number: z.string().min(1, "Número obrigatório"),
-  complement: z.string().optional(),
-  district: z.string().min(1, "Bairro obrigatório"),
-  state: z.string().min(1, "Estado obrigatório"),
-  city: z.string().min(1, "Cidade obrigatória"),
-  reference_point: z.string().optional(),
-  type: z
-    .array(z.enum(["Comercial", "Entrega", "Faturamento", "Residencial"]))
-    .min(1, "Selecione pelo menos um tipo")
-    .optional(),
+    zip_code: z.string().min(8, "CEP obrigatório").refine(isValidCEP, {
+        message: "CEP inválido.",
+    }),
+    street: z.string().min(1, "Logradouro obrigatório"),
+    number: z.string().min(1, "Número obrigatório"),
+    complement: z.string().optional(),
+    district: z.string().min(1, "Bairro obrigatório"),
+    state: z.string().min(1, "Estado obrigatório"),
+    city: z.string().min(1, "Cidade obrigatória"),
+    reference_point: z.string().optional(),
+    type: z
+        .array(z.enum(["Comercial", "Entrega", "Faturamento", "Residencial"]))
+        .min(1, "Selecione pelo menos um tipo")
+        .optional(),
 });
 
 export type AddressFormSchema = z.infer<typeof AddressFormSchema>;
 
 interface Props {
-  closeModal: () => void;
+    closeModal: () => void;
 }
 
 export function useAddressForm({ closeModal }: Props) {
-  const searchParams = useSearchParams();
-  const accountId = searchParams.get("id") ?? "";
+    const searchParams = useSearchParams();
+    const accountId = searchParams.get("id") ?? "";
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit: hookFormSubmit,
+        formState: { errors },
+        control,
+        setValue,
+        reset,
+    } = useForm<AddressFormSchema>({
+        resolver: zodResolver(AddressFormSchema),
+        defaultValues: {
+            zip_code: "",
+            street: "",
+            number: "",
+            complement: "",
+            district: "",
+            state: "",
+            city: "",
+            reference_point: "",
+            type: [],
+        },
+    });
 
-  const {
-    register,
-    handleSubmit: hookFormSubmit,
-    formState: { errors },
-    control,
-    setValue,
-    reset,
-  } = useForm<AddressFormSchema>({
-    resolver: zodResolver(AddressFormSchema),
-    defaultValues: {
-      zip_code: "",
-      street: "",
-      number: "",
-      complement: "",
-      district: "",
-      state: "",
-      city: "",
-      reference_point: "",
-      type: [],
-    },
-  });
+    const zip = useWatch({ control, name: "zip_code" });
+    const [loadingCep, setLoadingCep] = useState(false);
 
-  const zip = useWatch({ control, name: "zip_code" });
-  const [loadingCep, setLoadingCep] = useState(false);
+    useEffect(() => {
+        const fetchAddress = async (cep: string) => {
+            setLoadingCep(true);
 
-  useEffect(() => {
-    const fetchAddress = async (cep: string) => {
-      setLoadingCep(true);
+            try {
+                const res = await fetch(`/api/viacep?cep=${cep}`);
+                if (!res.ok) throw new Error("CEP não encontrado");
+                const data = await res.json();
+                setValue("street", data.street, { shouldValidate: true });
+                setValue("district", data.district, { shouldValidate: true });
+                setValue("city", data.city, { shouldValidate: true });
+                setValue("state", data.state, { shouldValidate: true });
+                if (data.complement) setValue("complement", data.complement);
+            } catch (e: any) {
+                toast({
+                    title: "Erro!",
+                    description: e.message,
+                    variant: "error",
+                });
+            } finally {
+                setLoadingCep(false);
+            }
+        };
+        if (isValidCEP(zip)) fetchAddress(zip);
+    }, [zip, setValue]);
 
-      try {
-        const res = await fetch(`/api/viacep?cep=${cep}`);
-        if (!res.ok) throw new Error("CEP não encontrado");
-        const data = await res.json();
-        setValue("street", data.street, { shouldValidate: true });
-        setValue("district", data.district, { shouldValidate: true });
-        setValue("city", data.city, { shouldValidate: true });
-        setValue("state", data.state, { shouldValidate: true });
-        if (data.complement) setValue("complement", data.complement);
-      } catch (e: any) {
-        toast({
-          title: "Erro!",
-          description: e.message,
-          variant: "error",
-        });
-      } finally {
-        setLoadingCep(false);
-      }
+    const handleSubmit = hookFormSubmit(
+        async (data) => {
+            try {
+                await createOneAddress({ ...data, accountId });
+                toast({
+                    title: "Sucesso!",
+                    description: "Endereço criado com sucesso!",
+                    variant: "success",
+                });
+                closeModal();
+                reset();
+                router.refresh();
+            } catch {
+                toast({
+                    title: "Erro!",
+                    description: "Falha ao registrar o endereço!",
+                    variant: "error",
+                });
+            }
+        },
+        (err) => {
+            console.log(err);
+        }
+    );
+
+    return {
+        register,
+        handleSubmit,
+        errors,
+        control,
+        loadingCep,
     };
-    if (isValidCEP(zip)) fetchAddress(zip);
-  }, [zip, setValue]);
-
-  const handleSubmit = hookFormSubmit(
-    async (data) => {
-      try {
-        await createOneAddress({ ...data, accountId });
-        toast({
-          title: "Sucesso!",
-          description: "Endereço criado com sucesso!",
-          variant: "success",
-        });
-        closeModal();
-        reset();
-      } catch {
-        toast({
-          title: "Erro!",
-          description: "Falha ao registrar o endereço!",
-          variant: "error",
-        });
-      }
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
-
-  return {
-    register,
-    handleSubmit,
-    errors,
-    control,
-    loadingCep,
-  };
 }
