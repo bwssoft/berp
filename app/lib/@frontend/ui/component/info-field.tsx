ca874355390e63c01840633ea4147f43068eb81f
Lauane Oliveira <oliveiralauane91@gmail.com>
2025-06-03 16:52:53 -0300

    feat: add CreateContact component and integrate it into the account page, enhance InfoField styling

--- app/lib/@frontend/ui/component/info-field.tsx
+++ app/lib/@frontend/ui/component/info-field.tsx
@@ -5,7 +5,7 @@ interface InfoFieldProps {
 
 export function InfoField({ label, value }: InfoFieldProps) {
   return (
-    <div className="text-xs">
+    <div className="text-xs mt-3">
       <h4 className="font-semibold">{label}</h4>
       <span>{value ?? ""}</span>
     </div>
