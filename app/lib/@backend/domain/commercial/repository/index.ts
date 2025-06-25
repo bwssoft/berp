4954a452fffa6d2e1942d685a58507d78a34d719
Lauane Oliveira <oliveiralauane91@gmail.com>
2025-06-25 11:45:03 -0300

    feat: add account attachment historical repository and update exports

--- app/lib/@backend/domain/commercial/repository/index.ts
+++ app/lib/@backend/domain/commercial/repository/index.ts
@@ -9,3 +9,4 @@ export * from "./contact.repository";
 export * from "./account.repository";
 export * from "./historical.repository";
 export * from "./account-attachment.object.repository";
+export * from "./account-attachment-historical.repository";
