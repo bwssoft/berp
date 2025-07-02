0809863df663eed8bcc898fbb0a93d9a14fc4c13
Lauane Oliveira <oliveiralauane91@gmail.com>
2025-07-02 15:58:03 -0300

    refactor: streamline CreateHistoricalForm and integrate CreateAnnexHistoricalModal in HistoricalDataPage

--- app/lib/@frontend/ui/page/commercial/account/data/historical.data.tsx
+++ app/lib/@frontend/ui/page/commercial/account/data/historical.data.tsx
@@ -1,6 +1,10 @@
 "use client"
 import { IHistorical } from "@/app/lib/@backend/domain";
 import { CreateHistoricalForm } from "../../../../form/commercial/account/historical/create/create.historical.form";
+import { TimelineItem } from "../../../../list/commercial/historical/time-line-item";
+import { CreateAnnexHistoricalModal } from "../../../../modal/comercial/account-attachments/create/annex-historical/annex-historical.create.commercial.modal";
+import { useCreateHistoricalForm } from "../../../../form/commercial/account/historical/create/use-create.historical.form";
+import { useCreateAnnexHistoricalModal } from "../../../../modal/comercial/account-attachments/create/annex-historical/use-annex-historical.create.commercial.modal";
 
 interface Props {
     historical: IHistorical[]
@@ -8,11 +12,32 @@ interface Props {
 }
 
 export function HistoricalDataPage({ historical, accountId }:Props) {
+  const {
+    handleDownload,
+    handleFileChange,
+    open
+  } = useCreateHistoricalForm({accountId})
+
+  const {closeModal} = useCreateAnnexHistoricalModal()
     return (
     <div className="flex flex-col items-center">
       <CreateHistoricalForm
         historical={historical ?? []}
         accountId={accountId}
+      />
+
+      <TimelineItem
+        onClickButtonDownload={(id: string, name: string) =>
+            handleDownload(id, name)
+        }
+        historical={historical}
+      />
+      
+      <CreateAnnexHistoricalModal
+          closeModal={closeModal}
+          onFileUploadSuccess={handleFileChange}
+          open={open}
+          accountId={accountId}
       />
     </div>
     )
