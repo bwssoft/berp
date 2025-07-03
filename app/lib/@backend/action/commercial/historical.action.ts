b7c3947c3077b4eba64645f6441f945a98b17f32
diego-dps <diego.pereira2806@hotmail.com>
2025-07-03 13:43:40 -0300

    feat: add revalidate path on historical

--- app/lib/@backend/action/commercial/historical.action.ts
+++ app/lib/@backend/action/commercial/historical.action.ts
@@ -1,19 +1,31 @@
-"use server"
+"use server";
 import { Filter } from "mongodb";
 import { IHistorical } from "../../domain";
-import { createOneHistoricalUsecase, findManyHistoricalUsecase } from "../../usecase";
+import {
+    createOneHistoricalUsecase,
+    findManyHistoricalUsecase,
+} from "../../usecase";
+import { revalidatePath } from "next/cache";
 
 export async function createOneHistorical(
-  historical: Omit<IHistorical, "id" | "created_at">
+    historical: Omit<IHistorical, "id" | "created_at">
 ) {
-  return await createOneHistoricalUsecase.execute(historical);
+    const result = await createOneHistoricalUsecase.execute(historical);
+    revalidatePath(`/commercial/account/management/historical?id={$accountId}`);
+
+    return result;
 }
 
 export async function findManyHistorical(
-  filter: Filter<IHistorical> = {},
-  page?: number,
-  limit?: number,
-  sort?: Record<string, 1 | -1>
+    filter: Filter<IHistorical> = {},
+    page?: number,
+    limit?: number,
+    sort?: Record<string, 1 | -1>
 ) {
-  return await findManyHistoricalUsecase.execute({ filter, page, limit, sort });
+    return await findManyHistoricalUsecase.execute({
+        filter,
+        page,
+        limit,
+        sort,
+    });
 }
--- app/lib/@frontend/ui/form/commercial/account/historical/create/use-create.historical.form.ts
+++ app/lib/@frontend/ui/form/commercial/account/historical/create/use-create.historical.form.ts
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { downloadAccountAttachmentHistorical } from "@/app/lib/@backend/action/commercial/account-attachment-historical.download.action";
 import { createOneHistorical } from "@/app/lib/@backend/action/commercial/historical.action";
@@ -12,109 +12,120 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const schema = z.object({
-  description: z.string().min(1, "Descrição é obrigatório")
-})
+    description: z.string().min(1, "Descrição é obrigatório"),
+});
 
 type CreateHistoricalFormSchema = z.infer<typeof schema>;
 
 type Props = {
-  accountId: string
-  closeModalAnnex?: () => void
-  file?: { name: string; url: string; id: string };
-}
+    accountId: string;
+    closeModalAnnex?: () => void;
+    file?: { name: string; url: string; id: string };
+};
 
-export function useCreateHistoricalForm({ accountId, closeModalAnnex, file }:Props) {
-
-  const {handleSubmit, register, formState: errors} = useForm<CreateHistoricalFormSchema>({
-    resolver: zodResolver(schema)
-  })
-  const [isLoading, setIsLoading] = useState(false)
-  const [selectContact, setSelectContact] = useState<ContactSelection>();
-
-  const {user} = useAuth()
-
-  const queryClient = useQueryClient()
-
-  const onSubmit = handleSubmit(async (data) => {
-    setIsLoading(true)
-     try {
-    await createOneHistorical({
-      ...data,
-      accountId: accountId,
-      title: "Histórico Manual de Conta",
-      type: "manual",
-      description: data.description,
-      author: {
-        name: user?.name ?? "",
-        avatarUrl: ""
-      },
-      file: file,
-      contacts: selectContact
-    })
-    } finally {
-        setIsLoading(false);
-        closeModalAnnex?.()
-    }
-  })
-
-  const handleDownload = async (id: string, name: string) => {
-    try {
-      if (!id) {
-        toast({
-          title: "Erro",
-          description: "ID do anexo não encontrado",
-          variant: "error",
-        });
-        return;
-      }
-
-      toast({
-        title: "Download iniciado",
-        description: `Baixando ${name}...`,
-        variant: "default",
-      });
-
-      const result = await downloadAccountAttachmentHistorical(id);
-
-      if (result.success && result.data) {
-        const uint8Array = new Uint8Array(result.data);
-        const blob = new Blob([uint8Array], { type: result.contentType });
-        const url = URL.createObjectURL(blob);
-
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = name;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        URL.revokeObjectURL(url);
-
-        toast({
-          title: "Sucesso",
-          description: `${name} baixado com sucesso`,
-          variant: "success",
-        });
-      } else {
-        throw new Error("Falha no retorno da API");
-      }
-    } catch (error) {
-      console.error("Error downloading file:", error);
-      toast({
-        title: "Erro",
-        description: "Falha ao fazer download do arquivo",
-        variant: "error",
-      });
-    }
-  };
-  
-  return {
-    onSubmit,
-    register,
+export function useCreateHistoricalForm({
+    accountId,
+    closeModalAnnex,
     file,
-    setSelectContact,
-    selectContact,
-    errors,
-    handleDownload,
-    isLoading
-  }
+}: Props) {
+    const {
+        handleSubmit,
+        register,
+        reset,
+        formState: errors,
+    } = useForm<CreateHistoricalFormSchema>({
+        resolver: zodResolver(schema),
+    });
+    const [isLoading, setIsLoading] = useState(false);
+    const [selectContact, setSelectContact] = useState<ContactSelection>();
+
+    const { user } = useAuth();
+
+    const queryClient = useQueryClient();
+
+    const onSubmit = handleSubmit(async (data) => {
+        setIsLoading(true);
+        try {
+            await createOneHistorical({
+                ...data,
+                accountId: accountId,
+                title: "Histórico Manual de Conta",
+                type: "manual",
+                description: data.description,
+                author: {
+                    name: user?.name ?? "",
+                    avatarUrl: "",
+                },
+                file: file,
+                contacts: selectContact,
+            });
+        } finally {
+            setIsLoading(false);
+            closeModalAnnex?.();
+            reset;
+        }
+    });
+
+    const handleDownload = async (id: string, name: string) => {
+        try {
+            if (!id) {
+                toast({
+                    title: "Erro",
+                    description: "ID do anexo não encontrado",
+                    variant: "error",
+                });
+                return;
+            }
+
+            toast({
+                title: "Download iniciado",
+                description: `Baixando ${name}...`,
+                variant: "default",
+            });
+
+            const result = await downloadAccountAttachmentHistorical(id);
+
+            if (result.success && result.data) {
+                const uint8Array = new Uint8Array(result.data);
+                const blob = new Blob([uint8Array], {
+                    type: result.contentType,
+                });
+                const url = URL.createObjectURL(blob);
+
+                const link = document.createElement("a");
+                link.href = url;
+                link.download = name;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+                URL.revokeObjectURL(url);
+
+                toast({
+                    title: "Sucesso",
+                    description: `${name} baixado com sucesso`,
+                    variant: "success",
+                });
+            } else {
+                throw new Error("Falha no retorno da API");
+            }
+        } catch (error) {
+            console.error("Error downloading file:", error);
+            toast({
+                title: "Erro",
+                description: "Falha ao fazer download do arquivo",
+                variant: "error",
+            });
+        }
+    };
+
+    return {
+        onSubmit,
+        register,
+        file,
+        setSelectContact,
+        selectContact,
+        errors,
+        handleDownload,
+        isLoading,
+    };
 }
