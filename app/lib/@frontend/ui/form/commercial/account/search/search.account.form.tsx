ec7b5bd8de1187408e6a3bc3b7cb6862205d636a
diego-dps <diego.pereira2806@hotmail.com>
2025-07-04 14:22:08 -0300

    refactor: adjust formatting and structure of the code in AccountFilterForm and account.columns

--- app/lib/@frontend/ui/form/commercial/account/search/search.account.form.tsx
+++ app/lib/@frontend/ui/form/commercial/account/search/search.account.form.tsx
@@ -4,142 +4,154 @@ import { useForm } from "react-hook-form";
 import { useTransition } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import {
-  Button,
-  Input,
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectItem,
+    Button,
+    Input,
+    Select,
+    SelectTrigger,
+    SelectValue,
+    SelectContent,
+    SelectItem,
 } from "../../../../component";
 import { useSectorModal } from "../../../../modal/comercial/sector";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "@/app/lib/@frontend/ui/component/form";
 
 type FilterFormData = {
-  client?: string;
-  document?: string;
-  sector?: string;
-  status?: string;
-  billingSituation?: string;
+    client?: string;
+    document?: string;
+    sector?: string;
+    status?: string;
+    billingSituation?: string;
 };
 
 export function AccountFilterForm() {
-  const form = useForm<FilterFormData>();
-  const [isPending, startTransition] = useTransition();
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const sectorModal = useSectorModal();
+    const form = useForm<FilterFormData>();
+    const [isPending, startTransition] = useTransition();
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const sectorModal = useSectorModal();
 
-  function onSubmit(data: FilterFormData) {
-    const params = new URLSearchParams(searchParams.toString());
+    function onSubmit(data: FilterFormData) {
+        const params = new URLSearchParams(searchParams.toString());
 
-    data.client ? params.set("client", data.client) : params.delete("client");
-    data.document
-      ? params.set("document", data.document)
-      : params.delete("document");
-    data.sector ? params.set("sector", data.sector) : params.delete("sector");
-    data.status && data.status !== "Todos"
-      ? params.set("status", data.status)
-      : params.delete("status");
-    data.billingSituation && data.billingSituation !== "Todos"
-      ? params.set("billingSituation", data.billingSituation)
-      : params.delete("billingSituation");
+        data.client
+            ? params.set("client", data.client)
+            : params.delete("client");
+        data.document
+            ? params.set("document", data.document)
+            : params.delete("document");
+        data.sector
+            ? params.set("sector", data.sector)
+            : params.delete("sector");
+        data.status && data.status !== "Todos"
+            ? params.set("status", data.status)
+            : params.delete("status");
+        data.billingSituation && data.billingSituation !== "Todos"
+            ? params.set("billingSituation", data.billingSituation)
+            : params.delete("billingSituation");
 
-    params.delete("page");
-    startTransition(() => {
-      router.push(`?${params.toString()}`);
-    });
-  }
+        params.delete("page");
+        startTransition(() => {
+            router.push(`?${params.toString()}`);
+        });
+    }
 
-  function handleClear() {
-    form.reset();
-    startTransition(() => {
-      router.push("?");
-    });
-  }
+    function handleClear() {
+        form.reset();
+        startTransition(() => {
+            router.push("?");
+        });
+    }
 
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="w-full border p-4 rounded shadow-sm space-y-4"
-      >
-        <div className="flex justify-between">
-          <h2 className="text-xl font-semibold">Conta</h2>
-        </div>
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-full border p-4 rounded shadow-sm space-y-4"
+            >
+                <div className="flex justify-between">
+                    <h2 className="text-xl font-semibold">Conta</h2>
+                </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {/* Cliente */}
-          <FormField
-            control={form.control}
-            name="client"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Cliente</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="Digite a Razão Social ou Nome Fantasia"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {/* Cliente */}
+                    <FormField
+                        control={form.control}
+                        name="client"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Cliente</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Digite a Razão Social ou Nome Fantasia"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-          {/* CPF/CNPJ */}
-          <FormField
-            control={form.control}
-            name="document"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>CPF/CNPJ</FormLabel>
-                <FormControl>
-                  <Input placeholder="Digite o CPF ou CNPJ" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                    {/* CPF/CNPJ */}
+                    <FormField
+                        control={form.control}
+                        name="document"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>CPF/CNPJ</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Digite o CPF ou CNPJ"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-          {/* Setor */}
-          <FormField
-            control={form.control}
-            name="sector"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Setor</FormLabel>
-                <FormControl>
-                  <Select
-                    onValueChange={field.onChange}
-                    value={field.value ?? ""}
-                  >
-                    <SelectTrigger>
-                      <SelectValue placeholder="Selecione o setor" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {sectorModal.enabledSectors.map((sector) => (
-                        <SelectItem key={sector.id} value={sector.name}>
-                          {sector.name}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                    {/* Setor */}
+                    <FormField
+                        control={form.control}
+                        name="sector"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Setor</FormLabel>
+                                <FormControl>
+                                    <Select
+                                        onValueChange={field.onChange}
+                                        value={field.value ?? ""}
+                                    >
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Selecione o setor" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            {sectorModal.enabledSectors.map(
+                                                (sector) => (
+                                                    <SelectItem
+                                                        key={sector.id}
+                                                        value={sector.name}
+                                                    >
+                                                        {sector.name}
+                                                    </SelectItem>
+                                                )
+                                            )}
+                                        </SelectContent>
+                                    </Select>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-          {/* Status Faturamento */}
-          <FormField
+                    {/* Status Faturamento */}
+                    {/* <FormField
             control={form.control}
             name="status"
             render={({ field }) => (
@@ -162,10 +174,10 @@ export function AccountFilterForm() {
                 <FormMessage />
               </FormItem>
             )}
-          />
+          /> */}
 
-          {/* Situação Faturamento */}
-          <FormField
+                    {/* Situação Faturamento */}
+                    {/* <FormField
             control={form.control}
             name="billingSituation"
             render={({ field }) => (
@@ -191,23 +203,27 @@ export function AccountFilterForm() {
                 <FormMessage />
               </FormItem>
             )}
-          />
-        </div>
+          /> */}
+                </div>
 
-        <div className="flex justify-end gap-3 pt-2">
-          <Button
-            type="button"
-            variant="outline"
-            onClick={handleClear}
-            disabled={isPending}
-          >
-            Limpar
-          </Button>
-          <Button type="submit" variant="default" disabled={isPending}>
-            {isPending ? "Buscando..." : "Pesquisar"}
-          </Button>
-        </div>
-      </form>
-    </Form>
-  );
+                <div className="flex justify-end gap-3 pt-2">
+                    <Button
+                        type="button"
+                        variant="outline"
+                        onClick={handleClear}
+                        disabled={isPending}
+                    >
+                        Limpar
+                    </Button>
+                    <Button
+                        type="submit"
+                        variant="default"
+                        disabled={isPending}
+                    >
+                        {isPending ? "Buscando..." : "Pesquisar"}
+                    </Button>
+                </div>
+            </form>
+        </Form>
+    );
 }
