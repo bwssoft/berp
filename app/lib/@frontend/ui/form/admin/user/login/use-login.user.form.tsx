6d7a3b0f0546c60ed2b5d64c5345cff247cf3b11
diego-dps <diego.pereira2806@hotmail.com>
2025-07-07 17:18:46 -0300

    feat: add FakeLoadingButton to LoginUserForm for improved loading state handling

--- app/lib/@frontend/ui/form/admin/user/login/use-login.user.form.tsx
+++ app/lib/@frontend/ui/form/admin/user/login/use-login.user.form.tsx
@@ -8,61 +8,60 @@ import { useSession } from "next-auth/react";
 import { authenticate } from "@/app/lib/@backend/action/auth/login.action";
 
 const schema = z.object({
-  username: z.string(),
-  password: z
-    .string()
-    .nonempty("É necessário informar a senha")
-    .min(6, "A senha deve ter no mínimo 8 caracteres")
-    .max(32, "A senha deve ter no máximo 32 caracteres")
-    .regex(/[A-Z]/, "A senha deve conter ao menos uma letra maiúscula")
-    .regex(/[a-z]/, "A senha deve conter ao menos uma letra minúscula")
-    .regex(/[0-9]/, "A senha deve conter ao menos um número")
-    .regex(
-      /[!@#$%^&*()_\-+=\[\]{};:'"\\|,.<>\/?`~]/,
-      "A senha deve conter ao menos um caractere especial"
-    ),
+    username: z.string(),
+    password: z
+        .string()
+        .nonempty("É necessário informar a senha")
+        .min(6, "A senha deve ter no mínimo 8 caracteres")
+        .max(32, "A senha deve ter no máximo 32 caracteres")
+        .regex(/[A-Z]/, "A senha deve conter ao menos uma letra maiúscula")
+        .regex(/[a-z]/, "A senha deve conter ao menos uma letra minúscula")
+        .regex(/[0-9]/, "A senha deve conter ao menos um número")
+        .regex(
+            /[!@#$%^&*()_\-+=\[\]{};:'"\\|,.<>\/?`~]/,
+            "A senha deve conter ao menos um caractere especial"
+        ),
 });
 
 type Schema = z.infer<typeof schema>;
 
 export function useLoginUserForm() {
-  const router = useRouter();
-  const { update } = useSession();
-  const {
-    register,
-    handleSubmit: hookFormSubmit,
-    formState: { errors },
-  } = useForm<Schema>({
-    resolver: zodResolver(schema),
-  });
+    const router = useRouter();
+    const { update } = useSession();
 
-  const handleSubmit = hookFormSubmit(async (data) => {
-    const login = await authenticate(data);
-    await update();
-    if (login.success) {
-      const session = await auth();
-      if (session?.user.temporary_password) {
-        router.push(`/set-password?id=${session.user.id}`);
-      } else {
-        router.push("/home");
-        toast({
-          title: "Sucesso!",
-          description: "Login realizado com sucesso.",
-          variant: "success",
-        });
-      }
-    } else {
-      toast({
-        title: "Erro!",
-        description: login.error ?? "Algo deu errado",
-        variant: "error",
-      });
-    }
-  });
+    const methods = useForm<Schema>({
+        resolver: zodResolver(schema),
+    });
 
-  return {
-    register,
-    handleSubmit,
-    errors,
-  };
+    const handleSubmit = methods.handleSubmit(async (data) => {
+        const login = await authenticate(data);
+        await update();
+
+        if (login.success) {
+            const session = await auth();
+            if (session?.user.temporary_password) {
+                router.push(`/set-password?id=${session.user.id}`);
+            } else {
+                router.push("/home");
+                toast({
+                    title: "Sucesso!",
+                    description: "Login realizado com sucesso.",
+                    variant: "success",
+                });
+            }
+        } else {
+            toast({
+                title: "Erro!",
+                description: login.error ?? "Algo deu errado",
+                variant: "error",
+            });
+        }
+    });
+
+    return {
+        methods,
+        handleSubmit,
+        register: methods.register,
+        errors: methods.formState.errors,
+    };
 }
