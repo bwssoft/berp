47a13878e3808a4ae66a2d7c1f22874da98ed5f1
diego-dps <diego.pereira2806@hotmail.com>
2025-07-11 10:16:58 -0300

    feat: refatorar exibição de botões e tooltips na coluna de ações do perfil

--- app/lib/@frontend/ui/table/admin/profile/profile.columns.tsx
+++ app/lib/@frontend/ui/table/admin/profile/profile.columns.tsx
@@ -54,67 +54,65 @@ export const columns = (props: Props): ColumnDef<IProfile>[] => [
                 props.restrictFeatureByProfile("admin:profile:view");
 
             return (
-                <TooltipProvider>
-                    <td className="flex gap-2 items-center">
-                        {hideViewButton && (
-                            <>
-                                <Button
-                                    title="Histórico"
-                                    onClick={() =>
-                                        props.openAuditModal({
-                                            id: original.id,
-                                            name: original.name,
-                                        })
-                                    }
-                                    className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                                >
-                                    <ClockIcon className="size-5" />
-                                </Button>
-                                <Button
-                                    title="Usuários"
-                                    onClick={() =>
-                                        props.openUserModal({
-                                            id: original.id,
-                                            name: original.name,
-                                        })
-                                    }
-                                    className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                                >
-                                    <UsersIcon className="size-5" />
-                                </Button>
-                            </>
-                        )}
+                <td className="flex gap-2 items-center">
+                    {hideViewButton && (
+                        <>
+                            <Button
+                                title="Histórico"
+                                onClick={() =>
+                                    props.openAuditModal({
+                                        id: original.id,
+                                        name: original.name,
+                                    })
+                                }
+                                className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                            >
+                                <ClockIcon className="size-5" />
+                            </Button>
+                            <Button
+                                title="Usuários"
+                                onClick={() =>
+                                    props.openUserModal({
+                                        id: original.id,
+                                        name: original.name,
+                                    })
+                                }
+                                className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                            >
+                                <UsersIcon className="size-5" />
+                            </Button>
+                        </>
+                    )}
 
-                        {hideActiveButton && (
-                            <Tooltip>
-                                <TooltipTrigger asChild>
-                                    <button
-                                        onClick={() =>
-                                            props.openActiveDialog(
-                                                original.id,
-                                                !original.active
-                                            )
+                    {hideActiveButton && (
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <button
+                                    onClick={() =>
+                                        props.openActiveDialog(
+                                            original.id,
+                                            !original.active
+                                        )
+                                    }
+                                >
+                                    <Toggle
+                                        value={original.active}
+                                        disabled={true}
+                                        title={(value) =>
+                                            value ? "Inativar" : "Ativar"
                                         }
-                                    >
-                                        <Toggle
-                                            value={original.active}
-                                            disabled={true}
-                                            title={(value) =>
-                                                value ? "Inativar" : "Ativar"
-                                            }
-                                            className="pointer-events-none"
-                                        />
-                                    </button>
-                                </TooltipTrigger>
-                                <TooltipContent side="top">
-                                    {original.active
-                                        ? "Inativar perfil"
-                                        : "Ativar perfil"}
-                                </TooltipContent>
-                            </Tooltip>
-                        )}
-                    </td>
-                </TooltipProvider>
+                                        className="pointer-events-none"
+                                    />
+                                </button>
+                            </TooltipTrigger>
+                            <TooltipContent side="top">
+                                {original.active
+                                    ? "Inativar perfil"
+                                    : "Ativar perfil"}
+                            </TooltipContent>
+                        </Tooltip>
+                    )}
+                </td>
             );
         },
     },
