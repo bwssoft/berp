26465480764211ea94e0f58ac262db76cfd5dbc5
Lauaneoc <oliveiralauane@gmail.com>
2025-07-15 15:25:46 -0300

    feat: add copy contact functionality and update contact from API

--- app/lib/@frontend/ui/card/commercial/account/contact.card.tsx
+++ app/lib/@frontend/ui/card/commercial/account/contact.card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { IContact } from "@/app/lib/@backend/domain";
-import { Phone, Mail, Edit, Star, Trash } from "lucide-react";
+import { Phone, Mail, Edit, Star, Trash, Copy } from "lucide-react";
 import {
   Avatar,
   AvatarFallback,
@@ -18,6 +18,11 @@ import {
 } from "../../../component/tooltip";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { cn } from "@/app/lib/util/cn";
+import { copyToClipboard } from "@/app/lib/util/copyToClipboard";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import { fetchCnpjData } from "@/app/lib/@backend/action/cnpja/cnpja.action";
+import { updateOneContact } from "@/app/lib/@backend/action/commercial/contact.action";
+import { toast } from "@/app/lib/@frontend/hook/use-toast";
 
 interface ContactCardProps {
   contact: IContact;
@@ -25,6 +30,7 @@ interface ContactCardProps {
   classname?: string;
   onClickEditContactButton: () => void
   onClickDeleteButton: () => void
+  onCopy: () => void
 }
 
 const getContactIcon = (
@@ -64,12 +70,77 @@ const formatContactValue = (
 };
 
 export default function ContactCard({ 
-  contact, 
-  accountId, 
+  contact,
+  accountId,
   classname,
   onClickEditContactButton, 
-  onClickDeleteButton 
+  onClickDeleteButton,
+  onCopy
 }: ContactCardProps) {
+
+  const handleCopy = () => {
+    const contactInfo = `${contact.name}\n` +
+      contact.contactItems.map(item => {
+        return `${item.type}: ${item.contact}`;
+      }).join("\n");
+
+    if (onCopy) {
+      onCopy();
+    } else {
+      copyToClipboard(contactInfo);
+    }
+  };
+
+  const handleReloadContactFromApi = async () => {
+    const data = await fetchCnpjData(contact.taxId!);
+
+    const newNumber = `${data?.phones[0].area}${data?.phones[0].number}`.replace(/\D/g, "");
+    const currentNumber = contact.contactItems[0].contact.replace(/\D/g, "");
+
+    if (newNumber !== currentNumber) {
+      const contactCnpj = await updateOneContact(
+        { id: contact.id },
+        {
+          accountId: accountId,
+          name: data?.company.name || data?.alias || "",
+          contractEnabled: false,
+          positionOrRelation: "",
+          originType: "api",
+          taxId: data?.taxId,
+          contactFor: ["Fiscal"],
+          contactItems: [
+            {
+              id: crypto.randomUUID(),
+              contact: `${data?.phones[0].area}${data?.phones[0].number}`,
+              type: data?.phones[0].type === "LANDLINE"
+                ? "Telefone Comercial"
+                : "Celular",
+              preferredContact: {},
+            },
+          ],
+        }
+      );
+
+      if (contactCnpj.success) {
+        toast({
+          title: "Contato atualizado",
+          description: "Os dados foram sincronizados com sucesso.",
+          variant: "success"
+        });
+      } else if (contactCnpj.error) {
+        toast({
+          title: "Erro",
+          description: contactCnpj.error.global || "Não foi possível atualizar o contato.",
+          variant: "error",
+        });
+      }
+    } else {
+      toast({
+        title: "Nenhuma atualização necessária",
+        description: "O contato já está sincronizado com a API.",
+      });
+    }
+  };
 
   return (
     <>
@@ -100,39 +171,79 @@ export default function ContactCard({
 
             <div className=" flex gap-2">
               <TooltipProvider>
+              {contact.originType !== "api" && (
+                <>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="h-8 w-8 text-muted-foreground hover:text-foreground"
+                        onClick={onClickEditContactButton}
+                        aria-label="Editar Contato"
+                      >
+                        <Edit className="h-4 w-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">
+                      <p>Editar contato</p>
+                    </TooltipContent>
+                  </Tooltip>
+
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="h-8 w-8 text-muted-foreground hover:text-foreground"
+                        onClick={onClickDeleteButton}
+                        aria-label="Excluir Contato"
+                      >
+                        <Trash className="h-4 w-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">
+                      <p>Excluir contato</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </>
+                )}
                 <Tooltip>
                   <TooltipTrigger asChild>
                     <Button
                       variant="ghost"
                       size="icon"
                       className="h-8 w-8 text-muted-foreground hover:text-foreground"
-                      onClick={onClickEditContactButton}
+                      onClick={handleCopy}
                       aria-label="Editar Contato"
                     >
-                      <Edit className="h-4 w-4" />
+                      <Copy className="h-4 w-4" />
                     </Button>
                   </TooltipTrigger>
                   <TooltipContent side="top">
-                    <p>Editar contato</p>
+                    <p>Copiar contato</p>
                   </TooltipContent>
                 </Tooltip>
-
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      className="h-8 w-8 text-muted-foreground hover:text-foreground"
-                      onClick={onClickDeleteButton}
-                      aria-label="Excluir Contato"
-                    >
-                      <Trash className="h-4 w-4" />
-                    </Button>
-                  </TooltipTrigger>
-                  <TooltipContent side="top">
-                    <p>Excluir contato</p>
-                  </TooltipContent>
-                </Tooltip>
+              {contact.originType == "api" && (
+                <>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="h-8 w-8 text-muted-foreground hover:text-foreground"
+                        onClick={handleReloadContactFromApi}
+                        aria-label="Editar Contato"
+                      >
+                        <ArrowPathIcon className="h-4 w-4" />
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent side="top">
+                      <p>Atualizar contato</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </>
+              )}
               </TooltipProvider>
             </div>
           </div>
