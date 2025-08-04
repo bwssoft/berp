956134e73b655412703d587ea59fd13ddccc5498
diego-dps <diego.pereira2806@hotmail.com>
2025-08-04 08:32:01 -0300

    refactor: standardize indentation and formatting in CreateContactAccountForm component

--- app/lib/@frontend/ui/form/commercial/account/contact/create/contact.create.account.form.tsx
+++ app/lib/@frontend/ui/form/commercial/account/contact/create/contact.create.account.form.tsx
@@ -2,12 +2,12 @@
 
 import { Controller } from "react-hook-form";
 import {
-  Button,
-  Checkbox,
-  Combobox,
-  ContactFormSchema,
-  Input,
-  useCreateContactAccount,
+    Button,
+    Checkbox,
+    Combobox,
+    ContactFormSchema,
+    Input,
+    useCreateContactAccount,
 } from "../../../../../component";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { ContactTable } from "@/app/lib/@frontend/ui/table/commercial/contact/table";
@@ -15,259 +15,274 @@ import { maskPhoneNumber } from "@/app/lib/util/mask-phone-number";
 import { formatCpf, formatRgOrCpf } from "@/app/lib/util/format-rg-cpf";
 
 type Props = {
-  closeModal?: () => void;
-  onSubmit: (data: ContactFormSchema, accountId?: string) => Promise<void>;
+    closeModal?: () => void;
+    onSubmit: (data: ContactFormSchema, accountId?: string) => Promise<void>;
 };
 
 export function CreateContactAccountForm({ closeModal, onSubmit }: Props) {
-  const {
-    control,
-    register,
-    watch,
-    fields,
-    handleSubmit,
-    handleNewContact,
-    handlePreferredContact,
-    handleRemove,
-    handleCheckboxChange,
-    setTempContact,
-    tempContact,
-    isLoading,
-    formState: { errors },
-  } = useCreateContactAccount(closeModal ?? (() => {}), onSubmit);
+    const {
+        control,
+        register,
+        watch,
+        fields,
+        handleSubmit,
+        handleNewContact,
+        handlePreferredContact,
+        handleRemove,
+        handleCheckboxChange,
+        setTempContact,
+        tempContact,
+        isLoading,
+        formState: { errors },
+    } = useCreateContactAccount(closeModal ?? (() => {}), onSubmit);
 
-  return (
-    <form
-      action={() => handleSubmit()}
-      className="flex flex-col items-start gap-4"
-    >
-      <Controller
-        name="contractEnabled"
-        control={control}
-        render={({ field }) => (
-          <Checkbox
-            checked={field.value}
-            onChange={field.onChange}
-            onBlur={field.onBlur}
-            name={field.name}
-            label="Contrato"
-          />
-        )}
-      />
+    type ContactType =
+        | "Celular"
+        | "Email"
+        | "Telefone Residencial"
+        | "Telefone Comercial";
 
-      <Input
-        placeholder="Digite o nome do contato"
-        label={"Nome"}
-        {...register("name")}
-        error={errors.name?.message}
-      />
-
-      <Input
-        placeholder="Digite o cargo ou relação"
-        label={"Cargo/Relação"}
-        {...register("positionOrRelation")}
-        error={errors.positionOrRelation?.message}
-      />
-
-      <Input
-        placeholder="Digite departamento"
-        label={"Área"}
-        {...register("department")}
-        error={errors.department?.message}
-      />
-
-      {watch("contractEnabled") && (
-        <>
-          <Input
-            placeholder="Digite o CPF do contato"
-            label={"CPF"}
-            {...register("cpf", {
-              onChange: (e) => {
-                e.target.value = formatCpf(e.target.value);
-              },
-            })}
-            error={errors.cpf?.message}
-          />
-          <Input
-            placeholder="Digite o RG do contato "
-            label={"RG / CIN"}
-            {...register("rg", {
-              onChange: (e) => {
-                e.target.value = formatRgOrCpf(e.target.value);
-              },
-            })}
-            error={errors.rg?.message}
-          />
-        </>
-      )}
-
-      <div className="flex gap-4 justify-between w-full items-end">
-        <Combobox
-          data={[
-            "Celular",
-            "Email",
-            "Telefone Residencial",
-            "Telefone Comercial",
-          ]}
-          onChange={([value]) => {
-            setTempContact((prev) => ({
-              ...prev,
-              type: value as typeof prev.type,
-            }));
-          }}
-          label="Tipo"
-          type="single"
-          placeholder="Selecione o tipo"
-          keyExtractor={(item) => item}
-          displayValueGetter={(item) => item}
-        />
-
-        <Input
-          label="Contato"
-          onChange={(e) => {
-            const value = e.target.value;
-            const formattedValue = [
-              "Celular",
-              "Telefone Residencial",
-              "Telefone Comercial",
-            ].includes(tempContact.type as string)
-              ? maskPhoneNumber(value, tempContact.type as string)
-              : value;
-
-            setTempContact((prev) => ({
-              ...prev,
-              contact: formattedValue,
-            }));
-          }}
-          value={tempContact.contact}
-          placeholder={
-            tempContact.type === "Celular"
-              ? "(00) 00000-0000"
-              : tempContact.type === "Telefone Residencial" ||
-                  tempContact.type === "Telefone Comercial"
-                ? "(00) 0000-0000"
-                : "Adicione o contato"
-          }
-          error={errors.contactItems?.message}
-        />
-
-        <div
-          className="bg-black text-white rounded-full p-1 mb-1.5 cursor-pointer"
-          onClick={handleNewContact}
+    return (
+        <form
+            action={() => handleSubmit()}
+            className="flex flex-col items-start gap-4"
         >
-          <PlusIcon className="text-white w-5 h-5" />
-        </div>
-      </div>
-
-      {fields.length > 0 && (
-        <ContactTable
-          data={fields}
-          handlePreferredContact={handlePreferredContact}
-          handleRemove={handleRemove}
-        />
-      )}
-
-      <h2>Contato para </h2>
-      <div className="flex gap-4">
-        <Controller
-          name={`contactFor`}
-          control={control}
-          render={({ field }) => (
-            <Checkbox
-              checked={field.value?.includes("Comercial")}
-              onChange={(e) =>
-                field.onChange(
-                  handleCheckboxChange(
-                    field.value,
-                    "Comercial",
-                    e.target.checked
-                  )
-                )
-              }
-              label="Comercial"
+            <Controller
+                name="contractEnabled"
+                control={control}
+                render={({ field }) => (
+                    <Checkbox
+                        checked={field.value}
+                        onChange={field.onChange}
+                        onBlur={field.onBlur}
+                        name={field.name}
+                        label="Contrato"
+                    />
+                )}
             />
-          )}
-        />
 
-        <Controller
-          name={`contactFor`}
-          control={control}
-          render={({ field }) => (
-            <Checkbox
-              checked={field.value?.includes("Suporte")}
-              onChange={(e) =>
-                field.onChange(
-                  handleCheckboxChange(field.value, "Suporte", e.target.checked)
-                )
-              }
-              label="Suporte"
+            <Input
+                placeholder="Digite o nome do contato"
+                label={"Nome"}
+                {...register("name")}
+                error={errors.name?.message}
             />
-          )}
-        />
 
-        <Controller
-          name={`contactFor`}
-          control={control}
-          render={({ field }) => (
-            <Checkbox
-              checked={field.value?.includes("Faturamento")}
-              onChange={(e) =>
-                field.onChange(
-                  handleCheckboxChange(
-                    field.value,
-                    "Faturamento",
-                    e.target.checked
-                  )
-                )
-              }
-              label="Faturamento"
+            <Input
+                placeholder="Digite o cargo ou relação"
+                label={"Cargo/Relação"}
+                {...register("positionOrRelation")}
+                error={errors.positionOrRelation?.message}
             />
-          )}
-        />
 
-        <Controller
-          name={`contactFor`}
-          control={control}
-          render={({ field }) => (
-            <Checkbox
-              checked={field.value?.includes("Marketing")}
-              onChange={(e) =>
-                field.onChange(
-                  handleCheckboxChange(
-                    field.value,
-                    "Marketing",
-                    e.target.checked
-                  )
-                )
-              }
-              label="Marketing"
+            <Input
+                placeholder="Digite departamento"
+                label={"Área"}
+                {...register("department")}
+                error={errors.department?.message}
             />
-          )}
-        />
-      </div>
 
-      <div>
-        {errors.contactFor && (
-          <p className="text-red-500 text-sm mt-1">
-            {errors.contactFor.message}
-          </p>
-        )}
-      </div>
+            {watch("contractEnabled") && (
+                <>
+                    <Input
+                        placeholder="Digite o CPF do contato"
+                        label={"CPF"}
+                        {...register("cpf", {
+                            onChange: (e) => {
+                                e.target.value = formatCpf(e.target.value);
+                            },
+                        })}
+                        error={errors.cpf?.message}
+                    />
+                    <Input
+                        placeholder="Digite o RG do contato "
+                        label={"RG / CIN"}
+                        {...register("rg", {
+                            onChange: (e) => {
+                                e.target.value = formatRgOrCpf(e.target.value);
+                            },
+                        })}
+                        error={errors.rg?.message}
+                    />
+                </>
+            )}
 
-      <div className="flex justify-end gap-4 w-full">
-        <Button
-          type="button"
-          variant={"ghost"}
-          onClick={closeModal}
-          disabled={isLoading}
-        >
-          Cancelar
-        </Button>
+            <div className="flex gap-4 justify-between w-full items-end">
+                <Combobox
+                    data={[
+                        "Celular",
+                        "Email",
+                        "Telefone Residencial",
+                        "Telefone Comercial",
+                    ]}
+                    label="Tipo"
+                    type="single"
+                    placeholder="Selecione o tipo"
+                    value={
+                        tempContact.type
+                            ? [tempContact.type as string]
+                            : undefined
+                    }
+                    onChange={(val) => {
+                        const selected = Array.isArray(val) ? val[0] : val;
+                        setTempContact((prev) => ({
+                            ...prev,
+                            type: selected as ContactType,
+                        }));
+                    }}
+                    keyExtractor={(item) => item}
+                    displayValueGetter={(item) => item}
+                />
+                <Input
+                    label="Contato"
+                    onChange={(e) => {
+                        const value = e.target.value;
+                        const formattedValue = [
+                            "Celular",
+                            "Telefone Residencial",
+                            "Telefone Comercial",
+                        ].includes(tempContact.type as string)
+                            ? maskPhoneNumber(value, tempContact.type as string)
+                            : value;
 
-        <Button type="submit" disabled={isLoading}>
-          {isLoading ? "Salvando..." : "Salvar"}
-        </Button>
-      </div>
-    </form>
-  );
+                        setTempContact((prev) => ({
+                            ...prev,
+                            contact: formattedValue,
+                        }));
+                    }}
+                    value={tempContact.contact}
+                    placeholder={
+                        tempContact.type === "Celular"
+                            ? "(00) 00000-0000"
+                            : tempContact.type === "Telefone Residencial" ||
+                                tempContact.type === "Telefone Comercial"
+                              ? "(00) 0000-0000"
+                              : "Adicione o contato"
+                    }
+                    error={errors.contactItems?.message}
+                />
+
+                <div
+                    className="bg-black text-white rounded-full p-1 mb-1.5 cursor-pointer"
+                    onClick={handleNewContact}
+                >
+                    <PlusIcon className="text-white w-5 h-5" />
+                </div>
+            </div>
+
+            {fields.length > 0 && (
+                <ContactTable
+                    data={fields}
+                    handlePreferredContact={handlePreferredContact}
+                    handleRemove={handleRemove}
+                />
+            )}
+
+            <h2>Contato para </h2>
+            <div className="flex gap-4">
+                <Controller
+                    name={`contactFor`}
+                    control={control}
+                    render={({ field }) => (
+                        <Checkbox
+                            checked={field.value?.includes("Comercial")}
+                            onChange={(e) =>
+                                field.onChange(
+                                    handleCheckboxChange(
+                                        field.value,
+                                        "Comercial",
+                                        e.target.checked
+                                    )
+                                )
+                            }
+                            label="Comercial"
+                        />
+                    )}
+                />
+
+                <Controller
+                    name={`contactFor`}
+                    control={control}
+                    render={({ field }) => (
+                        <Checkbox
+                            checked={field.value?.includes("Suporte")}
+                            onChange={(e) =>
+                                field.onChange(
+                                    handleCheckboxChange(
+                                        field.value,
+                                        "Suporte",
+                                        e.target.checked
+                                    )
+                                )
+                            }
+                            label="Suporte"
+                        />
+                    )}
+                />
+
+                <Controller
+                    name={`contactFor`}
+                    control={control}
+                    render={({ field }) => (
+                        <Checkbox
+                            checked={field.value?.includes("Faturamento")}
+                            onChange={(e) =>
+                                field.onChange(
+                                    handleCheckboxChange(
+                                        field.value,
+                                        "Faturamento",
+                                        e.target.checked
+                                    )
+                                )
+                            }
+                            label="Faturamento"
+                        />
+                    )}
+                />
+
+                <Controller
+                    name={`contactFor`}
+                    control={control}
+                    render={({ field }) => (
+                        <Checkbox
+                            checked={field.value?.includes("Marketing")}
+                            onChange={(e) =>
+                                field.onChange(
+                                    handleCheckboxChange(
+                                        field.value,
+                                        "Marketing",
+                                        e.target.checked
+                                    )
+                                )
+                            }
+                            label="Marketing"
+                        />
+                    )}
+                />
+            </div>
+
+            <div>
+                {errors.contactFor && (
+                    <p className="text-red-500 text-sm mt-1">
+                        {errors.contactFor.message}
+                    </p>
+                )}
+            </div>
+
+            <div className="flex justify-end gap-4 w-full">
+                <Button
+                    type="button"
+                    variant={"ghost"}
+                    onClick={closeModal}
+                    disabled={isLoading}
+                >
+                    Cancelar
+                </Button>
+
+                <Button type="submit" disabled={isLoading}>
+                    {isLoading ? "Salvando..." : "Salvar"}
+                </Button>
+            </div>
+        </form>
+    );
 }
