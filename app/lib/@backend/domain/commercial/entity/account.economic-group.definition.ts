cf7830f6535f407eb83c07d455e1b12c17542269
mateussatoh <mateussatoh@gmail.com>
2025-08-15 10:26:40 -0300

    refactor: clean up IAccount interface and add economic group definitions

---
+++ app/lib/@backend/domain/commercial/entity/account.economic-group.definition.ts
@@ -0,0 +1,10 @@
+export interface IAccountEconomicGroup {
+  id?: string | undefined;
+  economic_group_holding?: EconomicGroup;
+  economic_group_controlled?: EconomicGroup[];
+}
+
+export interface EconomicGroup {
+  name: string;
+  taxId: string;
+}
