e51769cb01bd6d7e721616fb7f2d8a88fde7ed60
Lauaneoc <oliveiralauane@gmail.com>
2025-09-03 12:45:02 -0300

    feat: add find-one usecase for price table

--- app/lib/@backend/action/commercial/price-table.action.ts
+++ app/lib/@backend/action/commercial/price-table.action.ts
@@ -3,6 +3,8 @@
 import { Filter } from "mongodb";
 import { IPriceTable } from "../../domain";
 import { findManyPriceTableUsecase } from "../../usecase/commercial/price-table/find-many.price-table.usecase";
+import { findOnePriceTableUsecase } from "../../usecase/commercial/price-table/find-one.price-table.usecase";
+import { PaginationResult } from "../../domain/@shared/repository/pagination.interface";
 
 export async function findManyAccount(
     filter: Filter<IPriceTable> = {},
@@ -11,4 +13,17 @@ export async function findManyAccount(
     sort?: Record<string, 1 | -1>
 ) {
     return await findManyPriceTableUsecase.execute({ filter, page, limit, sort });
+}
+
+export const findOnePriceTable = async (filter: Filter<IPriceTable>) => {
+  return await findOnePriceTableUsecase.execute(filter);
+};
+
+export async function findManySector(input: {
+  filter?: Filter<IPriceTable>;
+  page?: number;
+  limit?: number;
+  sort?: Record<string, 1 | -1>;
+}): Promise<PaginationResult<IPriceTable>> {
+  return await findManyPriceTableUsecase.execute(input);
 }
