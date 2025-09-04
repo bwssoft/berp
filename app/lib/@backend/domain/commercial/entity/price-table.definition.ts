b12d0f5982a6b8e53048c6d3d44158da66e8ef67
Lauaneoc <oliveiralauane@gmail.com>
2025-09-04 10:21:51 -0300

    feat: add publishPriceTable usecase

--- app/lib/@backend/domain/commercial/entity/price-table.definition.ts
+++ app/lib/@backend/domain/commercial/entity/price-table.definition.ts
@@ -1,4 +1,4 @@
-export type StatusPriceTable = "ativa" | "inativa" | "cancelada";
+export type StatusPriceTable = "ativa" | "inativa" | "cancelada" | "aguardando publicação" | "tabela programada para publicação" | "rascunho";
 
 export interface IPriceTable {
   id?: string;
@@ -6,7 +6,7 @@ export interface IPriceTable {
   startDateTime: Date;
   status?: StatusPriceTable;
   endDateTime: Date;
-  isTemporary: boolean;
+  isTemporary: boolean; // tabela provisoria // tabela normal
   conditionGroupIds: string[];
   enabledProductsIds: string[];
   created_at?: Date;
