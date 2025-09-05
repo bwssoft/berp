d03e378ba1daf309541e493d487225dd36e4bc16
Lauaneoc <oliveiralauane@gmail.com>
2025-09-05 14:30:21 -0300

    feat: add priceTable audit modal

---
+++ app/lib/@frontend/ui/modal/comercial/price-table/audit.price-table.modal.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { IAudit, IPriceTable } from "@/app/lib/@backend/domain";
+import { Modal, ModalBody, ModalContent } from "../../../component/modal";
+import { PaginationResult } from "@/app/lib/@backend/domain/@shared/repository/pagination.interface";
+import { AuditTable } from "../../../table/admin/audit/audit.table";
+
+
+interface Props {
+  open: boolean;
+  closeModal: () => void;
+  priceTable?: Pick<IPriceTable, "id" | "name">;
+  audits: PaginationResult<IAudit>;
+  currentPage: number;
+  handlePageChange: (page: number) => void;
+  isLoading?: boolean;
+}
+
+export function AuditPriceTableModal({
+  open,
+  closeModal,
+  priceTable,
+  audits,
+  currentPage,
+  handlePageChange,
+  isLoading,
+}: Props) {
+  return (
+    <Modal
+      position="center"
+      title={`Histórico de alterações da Tabela de Preço - ${priceTable?.name}`}
+      open={open}
+      onClose={closeModal}
+    >
+      <ModalBody>
+        <ModalContent>
+          <AuditTable
+            data={audits}
+            currentPage={currentPage}
+            handlePageChange={handlePageChange}
+            isLoading={isLoading}
+          />
+        </ModalContent>
+      </ModalBody>
+    </Modal>
+  );
+}
