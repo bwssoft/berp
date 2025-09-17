95787905de1c841a4ab67543110abc48faafa7ee
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2025-09-17 09:31:10 -0300

    feat: add new search configuration log form

--- app/lib/@frontend/ui/form/production/configuration-log/search/search.configuration-log.form.tsx
+++ app/lib/@frontend/ui/form/production/configuration-log/search/search.configuration-log.form.tsx
@@ -5,23 +5,65 @@ import { Button, Input } from "../../../../component";
 import { Filter } from "mongodb";
 import { IConfigurationLog } from "@/app/lib/@backend/domain";
 import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
+import { LoaderIcon, SearchIcon } from "lucide-react";
+import { DateRangeInput } from "../../../../component/date-range-input";
+import { Controller } from "react-hook-form";
 
 interface Props {
   filter: Filter<IConfigurationLog>;
 }
 export function ConfigurationLogSearchForm(props: Props) {
   const { filter } = props;
-  const { handleQuickSearch, handleExport } = useConfigurationLogSearchForm();
+  const { searchForm, handleSubmit, handleExport, isPending } =
+    useConfigurationLogSearchForm();
 
   return (
-    <div className="w-full flex justify-between items-end">
-      <form>
+    <div className="w-full flex justify-between items-end pt-3">
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center !justify-normal w-full gap-2"
+      >
         <Input
-          onChange={handleQuickSearch}
-          label="Pesquisar registros de configuração"
-          placeholder="Busque pelo equipamento, cliente, usuário, tecnologia e perfil."
-          className="sm:w-96"
+          {...searchForm.register("query")}
+          placeholder="Serial ou nome do usuário"
+          containerClassname="w-60"
         />
+
+        <Controller
+          control={searchForm.control}
+          name="created_at"
+          render={({ field }) => (
+            <DateRangeInput
+              value={{
+                from: field.value?.from
+                  ? new Date(field.value?.from)
+                  : undefined,
+                to: field.value?.to ? new Date(field.value?.to) : undefined,
+              }}
+              showTimeInputs
+              onChange={(date) => {
+                const { from, to } = date;
+                field.onChange({
+                  from,
+                  to,
+                });
+              }}
+            />
+          )}
+        />
+
+        <Button
+          disabled={isPending}
+          type="submit"
+          size="icon"
+          variant="outline"
+        >
+          {isPending ? (
+            <LoaderIcon className="animate-spin" size={12} />
+          ) : (
+            <SearchIcon size={12} />
+          )}
+        </Button>
       </form>
       <Button
         variant="outline"
--- app/lib/@frontend/ui/form/production/configuration-log/search/use-search.configuration-log.form.ts
+++ app/lib/@frontend/ui/form/production/configuration-log/search/use-search.configuration-log.form.ts
@@ -1,26 +1,61 @@
 import { exportConfigurationLog } from "@/app/lib/@backend/action/production/configuration-log.action";
 import { IConfigurationLog } from "@/app/lib/@backend/domain";
-import { useDebounce } from "@/app/lib/@frontend/hook/use-debounce";
 import { useHandleParamsChange } from "@/app/lib/@frontend/hook/use-handle-params-change";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Filter } from "mongodb";
-import { ChangeEvent } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  query: z.string().optional(),
+  created_at: z
+    .object({
+      from: z.date().optional(),
+      to: z.date().optional(),
+    })
+    .optional(),
+});
+
+type SearchConfigurationLogFormData = z.infer<typeof schema>;
 
 export const useConfigurationLogSearchForm = () => {
   const { handleParamsChange } = useHandleParamsChange();
 
-  const handleQuickSearch = useDebounce((e: ChangeEvent<HTMLInputElement>) => {
-    handleParamsChange({
-      query: e.target.value,
-    });
-  }, 200);
+  const [isPending, startTransition] = React.useTransition();
+
+  const searchForm = useForm<SearchConfigurationLogFormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleSubmit = searchForm.handleSubmit(
+    handleSucceededSubmit,
+    handleFailedSubmit
+  );
 
   const handleExport = async (filter: Filter<IConfigurationLog>) => {
     const url = await exportConfigurationLog(filter);
     window.location.href = url;
   };
 
+  function handleSucceededSubmit(data: SearchConfigurationLogFormData) {
+    startTransition(() => {
+      handleParamsChange({
+        query: data.query,
+        from: data.created_at?.from?.toISOString(),
+        to: data.created_at?.to?.toISOString(),
+      });
+    });
+  }
+
+  function handleFailedSubmit(error: any) {
+    console.log("🚀 ~ handleFailedSubmit ~ error:", error);
+  }
+
   return {
-    handleQuickSearch,
+    isPending,
+    searchForm,
     handleExport,
+    handleSubmit,
   };
 };
